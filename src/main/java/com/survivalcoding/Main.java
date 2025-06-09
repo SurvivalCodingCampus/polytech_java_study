1b0e115ac6acd4c244c45aac5a28716985eff39b
ari <bebe9349@naver.com>
2025-06-09 15:17:10 +0900

    TIL-2025.06.09.md

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -2,14 +2,24 @@ package com.survivalcoding;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class  Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        int ii =10;
+        boolean isMarried =true;
+        double d=5.0;
+        float f= 2.5f;
+        String name = "홍길동";
+
+        //sout 엔터
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+    public int attack () {
+        return 10;
     }
 }
