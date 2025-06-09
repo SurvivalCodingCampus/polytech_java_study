1fc25471d75d01e932d564d852cda09882ae9a7b
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-06-09 15:10:18 +0900

    feat : 변수 선언 개선

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -6,9 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         //sout (enter) 자동 완성
-        int ii =10;
-        boolean isarried = false;
-        double  d =5.0f;
 
 
         for (int i = 1; i <= 5; i++) {
