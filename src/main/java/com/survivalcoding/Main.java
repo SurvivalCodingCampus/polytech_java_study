70ac8c3f57ec841871bb697c91c087713119c5a8
choiain <csh25tmd25@gmail.com>
2025-06-09 15:53:11 +0900

    TIL 용어정리

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -2,6 +2,14 @@ package com.survivalcoding;
 
 public class Main {
     public static void main(String[] args) {
+        int ii = 10;
+        boolean isMarried = true;
+        double d = 5.0;
+        float f = 2.5f;
+        String name = "";
+
+        System.out.print("Hello and welcome!");
+
         Kinoko kinoko = new Kinoko();
         kinoko.setHp(100);
 
