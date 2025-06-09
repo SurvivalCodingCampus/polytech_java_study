1bdf3c4d51edbc484d541dfbf9ce576151cc3cc4
choiain <csh25tmd25@gmail.com>
2025-06-09 15:53:11 +0900

    TIL 용어정리

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -4,6 +4,12 @@ package com.survivalcoding;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int ii = 10;
+        boolean isMarried = true;
+        double d = 5.0;
+        float f = 2.5f;
+        String name = "";
+
         System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
