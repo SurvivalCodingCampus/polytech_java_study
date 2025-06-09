10e8d84ef6517fe8a2cd0d1f98b5336f86231693
jinsukuku <jinsukuku@gmail.com>
2025-06-09 14:34:38 +0900

    Practice: Java 타입별 변수 사용하기

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -6,10 +6,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
 
+        int n = 10;
+        boolean is = true;
+        double d = 1.2;
+        float f = 2.2f;
+        String test = "test";
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            System.out.println();
         }
     }
 }
