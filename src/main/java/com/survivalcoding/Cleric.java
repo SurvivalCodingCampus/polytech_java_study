26774a23152405677fb8607c4415c7422f29f17e
ari <bebe9349@naver.com>
2025-06-10 14:15:21 +0900

    성직자 클레릭 표현하는 클래스 작성

---
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -0,0 +1,14 @@
+package com.survivalcoding;
+
+
+import java.util.Random;
+
+public class Cleric {
+    String name;
+    int MAX_HP = 50;
+    int MAX_MP = 10;
+    int mp = MAX_MP;
+    int hp = MAX_HP;
+
+
+}
