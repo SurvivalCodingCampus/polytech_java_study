9851accd55e0a6f6728d8df40e48c257a422e2d3
MJ37031 <rlaalswl0520@naver.com>
2025-06-10 14:22:57 +0900

    feat: 연습문제 1

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -0,0 +1,21 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+public class Cleric { //용사
+        String name;                 //이름
+        int hp = 50;                      //HP
+        final int max_hp = 50;            //최대HP
+        int mp = 10;                      //MP
+        final int max_mp = 10;            //최대MP
+
+        int
+
+        void attack()  {}            //싸우기
+        void run()  {}               //도망
+        void sit(int sec)  {}        //앉기
+        void slip()  {}              //넘어지기
+        void sleep()  {}             //잠자기
+
+
+
+
+}
