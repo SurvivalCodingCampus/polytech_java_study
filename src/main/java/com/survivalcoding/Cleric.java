50bc6d846d7cc5f11a7e465a8f5d452d140732dd
morningB <yjw001205@gmail.com>
2025-06-10 12:25:23 +0900

    fix : Cleric pray mp 최대치 넘기는 코드 수정

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Cleric {
     String name;
-    int currentHp;
     final int maxHp = 50;
-    int currentMp;
     final int maxMp = 10;
+    int currentHp = maxHp; // 초기 값으로 할당
+    int currentMp = maxMp; // 초기 값으로 할당
+
     void selfAid(){
         if(currentMp < 5){
             // 스킬 못 쓴다고 하기
@@ -18,4 +19,23 @@ public class Cleric {
         this.currentHp  = maxHp;
     }
 
+    int pray(int time){
+        if(time < 0) {
+            // 잘못된 time 값
+            return 0;
+        }
+
+        Random rand = new Random();
+        int randomUpMp = rand.nextInt(3) + time;
+        int tempMp = this.currentMp;
+
+        currentMp += randomUpMp;
+
+        if(currentMp > maxMp){
+            currentMp = maxMp;
+            return maxMp - tempMp;
+        }
+        else
+            return randomUpMp;
+    }
 }
