0df58da4d3978a6d3741c8dd2a90763d369285a0
Lee Soohyeon <111167280+LeeSoohyeon-dev@users.noreply.github.com>
2025-06-10 14:18:09 +0900

    test : Cleric Test 수정

--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -12,14 +12,36 @@ class ClericTest {
     @DisplayName("Cleric 생성")
     void test() {
         final Cleric cleric = new Cleric("Cleric1");
+        assertEquals("Cleric1", cleric.name);
+        assertEquals(cleric.maxHp, cleric.hp);
+        assertEquals(cleric.maxMp, cleric.mp);
+    }
+
+    @Test
+    @DisplayName("cleric.selfAid()")
+    void selfAidTest() {
+        final Cleric cleric = new Cleric("Cleric1");
 
         cleric.selfAid();
 
         assertEquals(50, cleric.hp);
         assertEquals(5, cleric.mp);
+    }
 
-        cleric.pray(2);
-        assertTrue(cleric.mp >= 7 && cleric.mp <=10 );
+    @Test
+    @DisplayName("cleric.pray()")
+    void prayTest() {
+        final Cleric cleric1 = new Cleric("Cleric1");
+        cleric1.mp = 5;
+        cleric1.pray(2);
+        assertTrue(cleric1.mp >= 7 && cleric1.mp <=10 );
+        final Cleric cleric2 = new Cleric("Cleric2");
+
+        cleric2.mp = 9;
+        cleric2.pray(3);
+        assertEquals(10, cleric2.mp);
 
     }
+
+
 }
