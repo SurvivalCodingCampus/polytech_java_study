915f3affe7b182bf63901071ea14e10c7ca831f0
jinsukuku <jinsukuku@gmail.com>
2025-06-10 13:16:24 +0900

    Chore: add Kinoko.java

---
+++ src/main/java/com/survivalcoding/Kinoko.java
@@ -0,0 +1,11 @@
+package com.survivalcoding;
+
+// Kinoko 몬스터 클래스
+public class Kinoko{
+    int hp;
+    final int level= 10; // 필드(field)를 상수로 선언
+
+    void setHp(int hp){
+        this.hp = hp;
+    }
+}
