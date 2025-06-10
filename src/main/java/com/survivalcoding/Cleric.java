ed87c55313f26f1825eb86827c44d80c5d2b96de
morningB <yjw001205@gmail.com>
2025-06-10 11:04:50 +0900

    feat : Cleric selfAid 메소드 추가

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -1,10 +1,19 @@
 package com.survivalcoding;
 
+import java.util.Random;
+
 public class Cleric {
     String name;
     int currentHp;
     final int maxHp = 50;
     int currentMp;
     final int maxMp = 10;
+    void selfAid(){
+        this.currentMp -= 5;
+        if(this.currentMp < 0)
+            this.currentMp = 0;
+
+        this.currentHp  = maxHp;
+    }
 
 }
