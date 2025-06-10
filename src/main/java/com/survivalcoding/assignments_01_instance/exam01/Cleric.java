e0fd1933fdbf4c438d6cf2cd9101c4be571a9f1b
Lee Soohyeon <111167280+LeeSoohyeon-dev@users.noreply.github.com>
2025-06-10 14:19:43 +0900

    fix : Cleric.pray 수정

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -25,7 +25,6 @@ public class Cleric {
 
     public void pray(int prayerDuration)
     {
-        Random random = new Random();
         int min = 0;
         int max = 2;
         int manaRecoveryAmount = prayerDuration + ((int) (Math.random() * (max - min + 1) + min));
