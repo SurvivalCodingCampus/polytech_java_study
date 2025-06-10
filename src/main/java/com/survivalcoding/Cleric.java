217c645deef7b392cd85e82e6657aab8f2e5e503
ari <bebe9349@naver.com>
2025-06-10 14:17:17 +0900

    성직자 클레릭 표현하는 클래스 작성 상수로서 선언을 추가하였습니다.

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Cleric {
     String name;
-    int MAX_HP = 50;
-    int MAX_MP = 10;
+    final int MAX_HP = 50;
+    final int MAX_MP = 10;
     int mp = MAX_MP;
     int hp = MAX_HP;
 
