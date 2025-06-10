78e3ec0e985cf992b35091ffebe9522ff0b9d250
Lee Soohyeon <111167280+LeeSoohyeon-dev@users.noreply.github.com>
2025-06-10 13:45:17 +0900

    feat : Cleric Class 정의

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -0,0 +1,20 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+import java.util.Random;
+
+public class Cleric {
+
+    String name;
+    public int hp;
+    public int mp;
+    public final int maxHp = 50;
+    public final int maxMp = 10;
+
+
+    public Cleric(String name) {
+        this.name = name;
+        this.hp = maxHp;
+        this.mp = maxMp;
+    }
+
+}
