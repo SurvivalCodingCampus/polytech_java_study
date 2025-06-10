7c50c009ab02ca4d422c4768ca1caed6a08fd2b5
jinsukuku <jinsukuku@gmail.com>
2025-06-10 13:38:39 +0900

    Feat: prey() 메소드 추가

--- src/main/java/com/survivalcoding/practice/Cleric.java
+++ src/main/java/com/survivalcoding/practice/Cleric.java
@@ -1,5 +1,7 @@
 package com.survivalcoding.practice;
 
+import java.util.Random;
+
 public class Cleric {
     String name;
     int hp = 50;
@@ -10,5 +12,20 @@ public class Cleric {
     public void selfAid(){
         mp -= 5;
         hp = maxHp;
+    }
+
+    public int pray(int sec){
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+
+        // 회복량 recoveryMp에 저장
+        int recoveryMp = rand.nextInt(3) + sec; // 0~2 랜덤값 + 기도 시간(s)
+
+        // maxMp 보다 더 회복하는가?
+        if(recoveryMp + mp > maxMp){
+            recoveryMp = maxMp - mp;
+        }
+
+        return recoveryMp;
     }
 }
