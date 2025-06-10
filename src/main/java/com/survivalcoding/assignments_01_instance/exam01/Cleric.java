a56061f1cc8c44ced7f7d5f0bd4648245affecd7
Jiyun.Jung <modogo3@gmail.com>
2025-06-10 16:11:53 +0900

    cleric pray

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -23,14 +23,18 @@ public class Cleric {
     //회복량은 기도한 시간(초)에 랜덤하게 0~2포인트 보정을 한 양(3초 기도하면 회복량은 3~5포인트) 최대 MP 초과 회복 불가
     //인수에 "기도할 시간(초)"를 지정할 수 있고, 리턴 값은 "실제로 회복된 MP양" 반환
     public int pray(int sec) {
-
+        int result=0;
         Random rand=new Random();
+
         int recovery=sec+rand.nextInt(3);
-        MP+=recovery;
+        int availableRecovery = MAX_MP - MP;
+
+        result=Math.min(recovery, availableRecovery);
+        MP+=result;
 
         if (MP > MAX_MP) {
             MP = MAX_MP;
         }
-        return recovery;
+        return result;
     }
 }
