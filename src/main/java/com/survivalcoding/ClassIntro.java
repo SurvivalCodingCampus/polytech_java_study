e93c3b8ee3ba4c6e3cac642c8a5c0444a6e53d1a
Kimjongyun <zla0729@gmail.com>
2025-06-10 11:32:32 +0900

    feat: Add Cleric Class

---
+++ src/main/java/com/survivalcoding/ClassIntro.java
@@ -0,0 +1,19 @@
+package com.survivalcoding;
+
+public class ClassIntro {
+    // 클래스에서 사용하는 const = final
+    // 소문자로 표현
+    final int level = 10;
+    char suffix;
+    // 자바 명명 규칙
+    // 클래스명 : 파스칼
+    // 필드 명 : 카멜
+    // 메소드 명 : 카멜
+
+    void run(){
+        System.out.println("1" + this.suffix + "2");
+    }
+
+    // test 코드 작성법
+    // src > 우클릭 > new > dir
+}
---
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -0,0 +1,10 @@
+package com.survivalcoding;
+
+public class Cleric {
+    String name;
+    int hp = 10, mp = 10;
+
+    // final = 자바 클래스 필드의 상수화
+    final int maxHp = 50;
+    final int maxMp = 50;
+}
