13816140eeebc99313187d8681df36543d7ab06f
Lee Soohyeon <111167280+LeeSoohyeon-dev@users.noreply.github.com>
2025-06-10 15:11:22 +0900

    test : Cleric Test 수정

--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -20,12 +20,18 @@ class ClericTest {
     @Test
     @DisplayName("cleric.selfAid()")
     void selfAidTest() {
-        final Cleric cleric = new Cleric("Cleric1");
+        final Cleric cleric1 = new Cleric("Cleric1");
 
-        cleric.selfAid();
+        cleric1.hp = 50;
+        cleric1.selfAid();
+        assertEquals(50, cleric1.hp);
+        assertEquals(5, cleric1.mp);
 
-        assertEquals(50, cleric.hp);
-        assertEquals(5, cleric.mp);
+        final Cleric cleric2 = new Cleric("Cleric2");
+        cleric2.hp = 10;
+        cleric2.mp = 2;
+        assertEquals(10, cleric2.hp);
+        assertEquals(2, cleric2.mp);
     }
 
     @Test
@@ -34,12 +40,17 @@ class ClericTest {
         final Cleric cleric1 = new Cleric("Cleric1");
         cleric1.mp = 5;
         cleric1.pray(2);
-        assertTrue(cleric1.mp >= 7 && cleric1.mp <=10 );
-        final Cleric cleric2 = new Cleric("Cleric2");
+        assertTrue(cleric1.mp >= 7 && cleric1.mp <= 9 );
 
+        final Cleric cleric2 = new Cleric("Cleric2");
         cleric2.mp = 9;
         cleric2.pray(3);
         assertEquals(10, cleric2.mp);
+
+        final Cleric cleric3 = new Cleric("Cleric3");
+        cleric3.mp = 3;
+        cleric3.pray(0);
+        assertEquals(3, cleric3.mp);
 
     }
 
