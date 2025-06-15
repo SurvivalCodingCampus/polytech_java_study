f77449ab19d509589636e80e274ef207da06ddee
choiain <csh25tmd25@gmail.com>
2025-06-16 08:42:19 +0900

    test code

---
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -0,0 +1,141 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClericTest {
+
+    @Test
+    @DisplayName("selfAid() 사용 시 MP가 충분하다면, MP 5가 소비되고 HP는 최대 HP로 회복되어야 한다.")
+    //selfAid() 메소드 추가 인수가 없고, 리턴 값도 없다.
+    void selfAid1() {
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.hp = 5;
+
+        // when
+        cleric.selfAid();
+
+        // then
+        assertEquals(50, cleric.hp);
+    }
+
+    @Test
+    @DisplayName("selfAid() 사용 시 MP가 부족하다면, HP가 회복되지 않고 MP도 감소하지 않아야 한다.")
+    void selfAid2() {
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.hp = 5;
+        cleric.mp = 4;
+
+        // when
+        cleric.selfAid();
+
+        // then
+        assertEquals(5, cleric.hp);
+    }
+
+    @Test
+    @DisplayName("selfAid() 사용 시 HP가 이미 최대치라면, HP는 변하지 않고 MP만 소비되어야 한다.")
+    void selfAid3() {
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.hp = 50;
+        cleric.mp = 6;
+
+        // when
+        cleric.selfAid();
+
+        // then
+        assertEquals(50, cleric.hp);
+        assertEquals(1, cleric.mp);
+
+    }
+
+
+    @Test
+    @DisplayName("pray() 사용 시 MP가 회복되어야 하며, 반환 값은 실제로 회복된 MP 양과 일치해야 한다.")
+    void pray1() {
+
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.mp = 2;
+
+        // when
+        cleric.pray(2);
+
+        // then
+        assertEquals(4, cleric.mp);
+
+    }
+
+    @Test
+    @DisplayName("pray()를 일정 시간 기도했을 때, 회복되는 MP 양이 '기도 시간'에서 '기도 시간 + 2' 사이의 랜덤 범위 내에 있어야 한다.")
+    void pray2() {
+
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.mp = 2;
+
+        // when
+        cleric.pray(3);
+
+        // then
+        assertEquals(7, cleric.mp);
+
+    }
+
+    @Test
+    @DisplayName("pray() 사용 시 MP는 Max_MP를 초과하여 회복될 수 없으며, Max_MP 까지만 회복되어야 한다.")
+    void pray3() {
+
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.mp = 8;
+
+        // when
+        cleric.pray(3);
+
+        // then
+        assertEquals(10, cleric.mp);
+
+    }
+
+    @Test
+    @DisplayName("pray() 사용 시 기도 시간(prayTimeSec)이 0이거나 음수일 경우, MP는 회복되지 않고 0을 반환해야 한다.")
+    void pray4() {
+
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.mp = 2;
+
+        // when
+        cleric.pray(0);
+
+        // then
+        assertEquals(2, cleric.mp);
+
+    }
+
+    @Test
+    @DisplayName("pray() 사용 시 MP가 이미 Max_MP 라면, MP는 변하지 않고 0을 반환해야 한다.")
+    void pray5() {
+
+        // given
+        final Cleric cleric = new Cleric();
+        cleric.mp = 10;
+
+        // when
+        cleric.pray(3);
+
+        // then
+        assertEquals(10, cleric.mp);
+
+    }
+
+
+}
