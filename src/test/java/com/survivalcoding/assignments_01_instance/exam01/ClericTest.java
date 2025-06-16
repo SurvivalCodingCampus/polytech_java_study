74aac0d9af1840b3bb979749b405df6682fbadfa
Jiyun.Jung <modogo3@gmail.com>
2025-06-16 16:47:33 +0900

    Cleric 생성자 추가

--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -9,7 +9,7 @@ class ClericTest {
     @Test
     @DisplayName("Cleric 생성시 hp는 50 mp 10")
     void testCreate(){
-        Cleric cleric=new Cleric();
+        Cleric cleric=new Cleric("아서스");
         assertEquals(50,cleric.MAX_HP);
         assertEquals(10,cleric.MAX_MP);
     }
@@ -17,7 +17,7 @@ class ClericTest {
     @Test
     @DisplayName("selfAid 사용으로 MP 5소모 HP 최대 회복")
     void selfAid(){
-        Cleric cleric=new Cleric();
+        Cleric cleric=new Cleric("아서스");
         cleric.HP=30;
         cleric.MP=5;
 
@@ -29,7 +29,7 @@ class ClericTest {
     @Test
     @DisplayName("MP가 부족할 경우 selfAid 실행")
     void NotEnoughMP(){
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.HP = 30;
         cleric.MP = 3;      // 부족한 MP
 
@@ -41,7 +41,7 @@ class ClericTest {
     @Test
     @DisplayName("MP가 최대일 때 pray 실행")
     void prayWhenMaxMP(){
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.MP = cleric.MAX_MP;
 
         int recovered = cleric.pray(3);
@@ -53,7 +53,7 @@ class ClericTest {
     @Test
     @DisplayName("pray 사용으로 MP 회복")
     void pray(){
-        Cleric cleric=new Cleric();
+        Cleric cleric=new Cleric("아서스");
         cleric.MP = 5;
 
         int recovered = cleric.pray(3);     // 3초 기도 → 3~5 회복 시도
