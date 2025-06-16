bf44c0edc0dac3befc9d5d4edc125b1250aa3036
Omary <tata0426@naver.com>
2025-06-16 19:09:04 +0900

    feat: Q2. Cleric 생성자 테스트

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -1,15 +1,41 @@
 package com.survivalcoding.assignments_01_instance.exam01;
 
+import java.sql.SQLOutput;
+
 public class Cleric {
 
     public String name = "Cleric";
+    public int hp;
+    public int mp;
 
     public static final int MAX_HP = 50;
     public static final int MAX_MP = 10;
 
-    public int HP=50;
-    public int MP=10;
 
+    public Cleric(String name, int hp, int mp) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    public Cleric(String name, int hp) {
+        this(name, hp, MAX_MP);
+    }
+
+    public Cleric(String name) {
+        this(name, MAX_HP, MAX_MP);
+    }
+
+    public static void main(String[] args) {
+        Cleric a = new Cleric("아서스", 40, 5);
+        Cleric b = new Cleric("아서스", 35);
+        Cleric c = new Cleric("아서스"); // 골라쓰기
+
+    }
 }
+
+
+
+
 
 
--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -7,11 +7,39 @@ public class ClericTest {
 
     @Test
     public void testInitialValues() {
-        Cleric c = new Cleric();
+        Cleric c = new Cleric("test");
 
-        assertEquals(Cleric.MAX_HP, c.HP);
-        assertEquals(Cleric.MAX_MP, c.MP);
+        assertEquals(Cleric.MAX_HP, c.hp);
+        assertEquals(Cleric.MAX_MP, c.mp);
     }
+
+    @Test
+    public void testA() {
+        Cleric c = new Cleric("아서스", 40, 5);
+        assertEquals("아서스", c.name);
+        assertEquals(40, c.hp);
+        assertEquals(5, c.mp);
+    }
+
+    @Test
+    public void testB() {
+        Cleric c = new Cleric("아서스", 35);
+        assertEquals("아서스", c.name);
+        assertEquals(35, c.hp);
+        assertEquals(Cleric.MAX_MP, c.mp);
+    }
+
+    @Test
+    public void testC() {
+        Cleric c = new Cleric("아서스");
+        assertEquals("아서스", c.name);
+        assertEquals(Cleric.MAX_HP, c.hp);
+        assertEquals(Cleric.MAX_MP, c.mp);
+    }
+
+
+
+
 }
 
 
