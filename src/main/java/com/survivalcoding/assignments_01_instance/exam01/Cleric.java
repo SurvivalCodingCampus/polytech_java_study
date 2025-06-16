b8945a8abd5532d8cf093e8641319df8413325dc
MJ37031 <rlaalswl0520@naver.com>
2025-06-16 16:02:52 +0900

    feat: Cleric 및 테스트 코드 작성

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Cleric.java
@@ -1,9 +1,64 @@
 package com.survivalcoding.assignments_01_instance.exam01;
 
+import java.util.Random;
+
 public class Cleric { //용사
-        String name;                 //이름
-        int hp = 50;                      //HP
-        final int max_hp = 50;            //최대HP
-        int mp = 10;                      //MP
-        final int max_mp = 10;            //최대MP
+    //연습문제1
+    static int max_hp = 50;
+    static int max_mp = 10;
+
+    String name;                 //이름
+    int hp;                      //HP
+    int mp;                      //MP
+    final Random rand = new Random();
+
+    void selfAid() {
+        if (mp >= 5 && hp < max_hp) { // HP가 최대가 아닐 때만 실행
+            mp -= 5;
+            hp = max_hp;
+        } else if (mp >= 5 && hp == max_hp) { // HP가 최대가 아닐 때만 실행
+            mp -= 5;
+        }
+
+    }
+
+    int pray(int sec) {
+        if (sec <= 0 || mp == max_mp) {
+            return 0;
+        } else {
+            int recover = sec + rand.nextInt(3); // 단일 보정값(0~2) 추가  sec = 10 -> recover = 10~12
+
+            int actualRecover = Math.min(recover, max_mp - mp);
+
+            mp += actualRecover;
+            return actualRecover;
+        }
+
+
+    }
+
+
+    // 이름, HP, MP 를 지정
+    Cleric(String name, int hp, int mp) {
+        this.hp = hp;
+        this.name = name;
+        this.mp = mp;
+    }
+
+    //이름과 HP만 지정, MP는 최대 MP와 같은 값이 초기화
+    Cleric(String name, int hp) {
+        /*this.name = name;
+        this.hp = hp;
+        this.mp = max_mp;*/
+        this(name, hp, max_mp);
+    }
+
+    //이름만 지정, HP 와 MP 는 최대 HP와 최대 MP로 초기화
+    Cleric(String name) {
+        /*this.name = name;
+        this.mp = max_mp;
+        this.hp = max_hp;*/
+        this(name, max_hp, max_mp);
+    }
+
 }
--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/ClericTest.java
@@ -3,13 +3,13 @@ package com.survivalcoding.assignments_01_instance.exam01;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClericTest {
     @Test
     @DisplayName("성직자 생성 시 HP, MP가 올바른 값으로 초기화되어야 한다.")
     void testCreate() {
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         assertEquals(50, cleric.hp);
         assertEquals(10, cleric.mp);
     }
@@ -18,7 +18,7 @@ class ClericTest {
     @DisplayName("selfAid() 사용 시 MP가 충분하다면, MP 5가 소비되고 HP는 최대 HP로 회복되어야 한다.,")
     void testselfAid() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.hp = 20;  // 현재 HP를 20으로 설정 (최대 HP 미만)
         cleric.mp = 8;   // 현재 MP를 8로 설정 (MP 충분)
 
@@ -26,7 +26,7 @@ class ClericTest {
         cleric.selfAid();
 
         //then
-        assertEquals(cleric.max_hp, cleric.hp);
+        assertEquals(Cleric.max_hp, cleric.hp);
         assertEquals(3, cleric.mp);
     }
 
@@ -34,7 +34,7 @@ class ClericTest {
     @DisplayName("selfAid() 사용 시 MP가 부족하다면, HP가 회복되지 않고 MP도 감소하지 않아야 한다.")
     void testselfAid2() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.hp = 20;  // 현재 HP를 20으로 설정 (최대 HP 미만)
         cleric.mp = 3;   // 현재 MP를 3으로 설정 (MP 부족)
 
@@ -50,7 +50,7 @@ class ClericTest {
     @DisplayName("selfAid() 사용 시 HP가 이미 최대치라면, HP는 변하지 않고 MP만 소비되어야 한다.")
     void testselfAid3() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.hp = 50;  // 현재 HP 최대
         cleric.mp = 10;   // 현재 MP를 10
 
@@ -58,7 +58,7 @@ class ClericTest {
         cleric.selfAid();
 
         //then
-        assertEquals(cleric.max_hp, cleric.hp);
+        assertEquals(Cleric.max_hp, cleric.hp);
         assertEquals(5, cleric.mp);
     }
 
@@ -66,7 +66,7 @@ class ClericTest {
     @DisplayName("pray() 사용 시 MP가 회복되어야 하며, 반환 값은 실제로 회복된 MP 양과 일치해야 한다. (MP가 최대치가 아닐 때)")
     void testspray() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.mp = 5; // MP를 최대치 미만으로 설정
         int beforeMp = cleric.mp;
         int sec = 3;
@@ -83,7 +83,7 @@ class ClericTest {
     @DisplayName("pray() 사용 시 MP는 MAX_MP를 초과하여 회복될 수 없으며, MAX_MP까지만 회복되어야 한다.")
     void testspray2() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.mp = 10; // MP를 최대치로 설정
         int beforeMp = cleric.mp;
         int sec = 3;
@@ -92,16 +92,15 @@ class ClericTest {
         int recovered = cleric.pray(sec);
 
         // then 증가한 mp 최대값 넘지 않음
-        assertEquals(beforeMp + recovered, cleric.max_mp);
+        assertEquals(beforeMp + recovered, Cleric.max_mp);
     }
-
 
 
     @Test
     @DisplayName("pray() 사용 시 MP는 MAX_MP를 초과하여 회복될 수 없으며, MAX_MP까지만 회복되어야 한다.")
     void testspray3() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스");
         cleric.mp = 5; // MP 설정
         int beforeMp = cleric.mp;
         int sec = 0;
@@ -119,8 +118,8 @@ class ClericTest {
     @DisplayName("pray() 사용 시 MP가 이미 MAX_MP라면, MP는 변하지 않고 0을 반환해야 한다.")
     void testspray4() {
         //given
-        Cleric cleric = new Cleric();
-        cleric.mp = cleric.max_mp; // MP 최댓값응로 설정
+        Cleric cleric = new Cleric("아서스");
+        cleric.mp = Cleric.max_mp; // MP 최댓값응로 설정
         int beforeMp = cleric.mp;
         int sec = 0;
 
@@ -132,5 +131,40 @@ class ClericTest {
         assertEquals(0, recovered);   // pray()는 0 반환
     }
 
+    @Test
+    @DisplayName("이름, HP, MP 를 지정하여 인스턴스화 할 수 있다")
+    void testClericnew1() {
+        //given
+        Cleric cleric = new Cleric("아서스", 40, 5);
+
+        // then
+        assertEquals("아서스", cleric.name);  //이름은 아서스
+        assertEquals(40, cleric.hp);   //hp = 40
+        assertEquals(5, cleric.mp);   //mp = 5
+    }
+
+    @Test
+    @DisplayName("이름, HP만 지정하여 인스턴스화 할 수 있다")
+    void testClericnew2() {
+        //given
+        Cleric cleric = new Cleric("아서스", 35);
+
+        // then
+        assertEquals("아서스", cleric.name);  //이름은 아서스
+        assertEquals(35, cleric.hp);   //hp = 35
+        assertEquals(10, cleric.mp);   //mp = 10
+    }
+
+    @Test
+    @DisplayName("이름만 지정하여 인스턴스화 할 수 있다")
+    void testClericnew3() {
+        //given
+        Cleric cleric = new Cleric("아서스");
+
+        // then
+        assertEquals("아서스", cleric.name);  //이름은 아서스
+        assertEquals(Cleric.max_hp, cleric.hp);   //hp = 최대
+        assertEquals(Cleric.max_mp, cleric.mp);   //mp = 최대
+    }
 
 }
