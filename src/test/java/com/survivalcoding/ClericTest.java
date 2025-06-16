1268cf28a2ae4dcf9b8522c27945286e272126b6
Soso-Kiki <polytechid10@gmail.com>
2025-06-16 15:56:01 +0900

    feat: 클래스 'Cleric' 최대 HP, MP 필드 공유 선언 및 생성자 추가
    - MaxHP, MaxMP 속성 static 선언해 공유 자원으로 변경
    - Cleric(String name, int hp, int mp) 생성자 정의
    - leric(String name, int hp) 생성자 정의 및 this("아서스", hp, maxMp)로 중복 코드 없도록 생성
    - public Cleric(String name) 생성자 저의 및 this("아서스", maxHp, maxMp)로 중복 코드 없도록 생성

--- src/test/java/com/survivalcoding/ClericTest.java
+++ src/test/java/com/survivalcoding/ClericTest.java
@@ -3,12 +3,14 @@ package com.survivalcoding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.Random;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ClericTest {
-    @Test @DisplayName("selfAid()를 하면 mp-5 하면 hp = maxhp가 된다")
-    void testselfAid(){
+
+    @Test
+    @DisplayName("selfAid()를 하면 mp-5 하면 hp = maxhp가 된다")
+    void testSelfAid() {
 
         //given(준비)
         final Cleric cleric = new Cleric("성직자");
@@ -17,35 +19,63 @@ class ClericTest {
         cleric.mp = 5;
         cleric.selfAid();
         //then(검증)
-        assertEquals(50,cleric.hp);
+        assertEquals(50, cleric.hp);
     }
 
     @Test
     @DisplayName("pray 확인")
-    void testpray() {
-
-
-        // test01 : 시간 초가 랜덤으로 잘 들어가는지
+    void testPray() { // test01 : 시간 초가 랜덤으로 잘 들어가는지
         //given(준비)
         final Cleric cleric01 = new Cleric("성직자");
         //when(실행)
         cleric01.mp = 5;
-        cleric01.pray(3);//maxmp =10
+        int value = cleric01.pray(3);//maxmp =10
         //then(검증)
-        assertTrue(cleric01.mp <=10 && cleric01.mp<=8);
+        assertTrue(cleric01.mp <= 10 && cleric01.mp >= 8);
 
         // test02 : 시간 초가 10일 때, mp = maxmp 가 나오는지
         //given(준비)
         final Cleric cleric02 = new Cleric("성직자");
         //when(실행)
-        cleric01.mp = 5;
+        cleric02.mp = 5;
         cleric02.pray(10);//maxmp =10
         //then(검증)
-        assertEquals(10,cleric02.mp);
-
-
-
-
+        assertEquals(10, cleric02.mp);
     }
+
+    @Test
+    @DisplayName("(A) 문제 테스트")
+    void testA() {
+        //given(준비)
+        final Cleric cleric = new Cleric("아서스", 40, 5);
+        //when(실행)
+        //then(검증)
+        assertEquals("아서스", cleric.name);
+        assertEquals(40, cleric.hp);
+        assertEquals(5, cleric.mp);
+        //assertTrue(cleric.hp == 40);
+    }
+
+    @Test
+    @DisplayName("(B) 문제 테스트")
+    void testB() {
+        //given(준비)
+        final Cleric cleric = new Cleric("아서스", 35);
+        //when(실행)
+        //then(검증)
+        assertEquals(10, cleric.mp);
+    }
+
+    @Test
+    @DisplayName("(C) 문제 테스트")
+    void testC() {
+        //given(준비)
+        final Cleric cleric = new Cleric("아서스");
+        //when(실행)
+        //then(검증)
+        assertEquals(50, cleric.hp);
+        assertEquals(10, cleric.mp);
+    }
+
 
 }
