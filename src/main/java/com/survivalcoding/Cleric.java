9ae910138c6ed6f4927491ce70b919ff8d34ad44
Kimjongyun <zla0729@gmail.com>
2025-06-16 14:13:47 +0900

    feat: maxHp & maxMp를 static으로

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -4,16 +4,19 @@ import java.lang.Math;
 
 public class Cleric {
     String name = "A";
-    int hp = 50, mp = 10;
+    int hp, mp;
 
     // final = 자바 클래스 필드의 상수화
-    final int maxHp = 50;
-    final int maxMp = 10;
+    static final int maxHp = 50;
+    static final int maxMp = 10;
 
     // 기도한 시간 + 랜덤 포인트만큼 mp회복
     Random random = new Random();
 
-    Cleric() {}
+    Cleric() {
+        this.hp = 50;
+        this.mp = 10;
+    }
 
     // 이름을 받으면 hp와 mp가 가득차게
     Cleric(String name) {
@@ -42,7 +45,7 @@ public class Cleric {
 
     // mp는 기도한 시간당 1만큼 + 추가 랜덤 포인트로 회복
     public int pray(int timePray) {
-        if((timePray <= 0) || (this.mp == this.maxMp)) {
+        if((timePray <= 0) || (this.mp == maxMp)) {
             return 0;
         }
 
---
+++ src/main/java/com/survivalcoding/Wizard.java
@@ -0,0 +1,10 @@
+package com.survivalcoding;
+
+public class Wizard {
+    String name;
+    int hp;
+
+    void heal(Hero hero) {
+        hero.hp += 10;
+    }
+}
