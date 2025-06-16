8322d02d41ca982593d57dab7e48982afca643bd
Kimjongyun <zla0729@gmail.com>
2025-06-16 17:12:10 +0900

    feat: 3개의 Constructor 추가

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -3,8 +3,9 @@ import java.util.Random;
 import java.lang.Math;
 
 public class Cleric {
-    String name = "A";
-    int hp, mp;
+    private String name = "A";
+    private int hp;
+    private int mp;
 
     // final = 자바 클래스 필드의 상수화
     static final int maxHp = 50;
@@ -13,16 +14,22 @@ public class Cleric {
     // 기도한 시간 + 랜덤 포인트만큼 mp회복
     Random random = new Random();
 
-    Cleric() {
-        this.hp = 50;
-        this.mp = 10;
+    Cleric(String name) {
+        this(name, Cleric.maxHp, Cleric.maxMp);
     }
 
-    // 이름을 받으면 hp와 mp가 가득차게
-    Cleric(String name) {
+    Cleric(String name, int hp) {
+        this(name, hp, Cleric.maxMp);
+    }
+
+    Cleric(String name, int hp, int mp) {
         this.name = name;
-        this.hp = maxHp;
-        this.mp = maxMp;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getHp() {
@@ -40,18 +47,18 @@ public class Cleric {
         }
 
         this.mp -= 5;
-        this.hp = maxHp;    // 체력이 이미 가득 차있으면 마나 소모만 됨
+        this.hp = Cleric.maxHp;    // 체력이 이미 가득 차있으면 마나 소모만 됨
     }
 
     // mp는 기도한 시간당 1만큼 + 추가 랜덤 포인트로 회복
     public int pray(int timePray) {
-        if((timePray <= 0) || (this.mp == maxMp)) {
+        if((timePray <= 0) || (this.mp == Cleric.maxMp)) {
             return 0;
         }
 
         int randomPoint = random.nextInt(3);
         int recovery = timePray + randomPoint;
-        int finalRecovery = Math.min(recovery, maxMp - this.mp);
+        int finalRecovery = Math.min(recovery, Cleric.maxMp - this.mp);
         this.mp += finalRecovery;
 
         return finalRecovery;
