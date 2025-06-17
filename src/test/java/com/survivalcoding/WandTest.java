05ea8067fb3585dd4026b5b72022e8b332335ad1
morningB <yjw001205@gmail.com>
2025-06-17 13:53:39 +0900

    test: Wand 생성자 추가

--- src/test/java/com/survivalcoding/WandTest.java
+++ src/test/java/com/survivalcoding/WandTest.java
@@ -1,22 +1,41 @@
 package com.survivalcoding;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WandTest {
 
     @Test
-    void getName() {
+    @DisplayName("getter 테스트")
+    void setter1() {
+        Wand wand = new Wand("g1", 20);
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setName(null);
+        });
     }
 
     @Test
-    void setName() {
+    @DisplayName("getter 테스트")
+    void setter2() {
+        Wand wand = new Wand("g2", 30);
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setPower(0);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setPower(1110);
+        });
     }
 
     @Test
-    void getPower() {
-    }
+    @DisplayName("getter 테스트")
+    void getter1() {
+        Wand wand = new Wand("getter", 10);
 
-    @Test
-    void setPower() {
+        assertEquals("gett2er", wand.getName());
+        assertEquals(110, wand.getPower());
+
     }
 }
