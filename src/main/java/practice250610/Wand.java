655578d8b6b1d91e1539100e9c2c6c6070f227da
Dayeong <https://github.com/iiijjjijij>
2025-06-17 13:27:47 +0900

    feat: setPower()에 타당성 검사 추가

--- src/main/java/practice250610/Wand.java
+++ src/main/java/practice250610/Wand.java
@@ -23,6 +23,12 @@ public class Wand {
     }
 
     public void setPower(double power) {
+        if (power < 0.5) {
+            throw new IllegalArgumentException("파워가 0.5 이상이어야 함");
+        }
+        if (power > 100.0) {
+            throw new IllegalArgumentException("파워가 100.0 이하여야 함");
+        }
         this.power = power;
     }
 }
