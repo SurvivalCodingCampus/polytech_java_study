129db3b5ffe6c8100f4fd8eb6a91d9cf4d28a199
Hae1Won1 <h121581@naver.com>
2025-06-17 14:37:44 +0900

    feat: Person class 작성 및 테스트

---
+++ src/main/java/com/survivalcoding/class_3/Person.java
@@ -0,0 +1,23 @@
+package com.survivalcoding.class_3;
+
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Person {
+    String name;
+    int birthYear;
+
+    public Person(String name, int birthYear){
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public int getAge(){
+        //Date now = new Date();
+        Calendar calendar = Calendar.getInstance();
+        //calendar.setTime(now);
+        int age = calendar.get(Calendar.YEAR) - this.birthYear + 1;
+        return age;
+    }
+}
---
+++ src/test/java/com/survivalcoding/class_3/PersonTest.java
@@ -0,0 +1,15 @@
+package com.survivalcoding.class_3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    @DisplayName("age 반환 테스트")
+    void returnAge(){
+        Person person = new Person("해원", 2000);
+        assertEquals(26,person.getAge());
+    }
+}
