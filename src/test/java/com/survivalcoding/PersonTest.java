cca61f34d10212a5a594a40753411c0d475f462f
morningB <yjw001205@gmail.com>
2025-06-17 13:53:34 +0900

    test: Person 생성자 추가

---
+++ src/test/java/com/survivalcoding/PersonTest.java
@@ -0,0 +1,28 @@
+package com.survivalcoding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PersonTest {
+    @Test
+    void testConstructor() {
+        Person person = new Person("가나초콜릿", 2000);
+
+        assertEquals("가나초콜릿", person.getName());
+        assertEquals(2000, person.getBirthYear());
+        assertEquals(25, person.getAge());
+    }
+
+    @Test
+    void testConstructor2() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Person(null, -101);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Person("awd", -101);
+        });
+    }
+}
