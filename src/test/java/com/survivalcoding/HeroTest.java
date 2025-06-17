47b610c795a1990802bd5ff8a7781e06ebf0a409
Kimjongyun <zla0729@gmail.com>
2025-06-17 13:12:44 +0900

    test: HeroTest 추가

---
+++ src/test/java/com/survivalcoding/HeroTest.java
@@ -0,0 +1,24 @@
+package com.survivalcoding;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    void heroTest() {
+        Hero hero = new Hero("영웅", 100);
+        assertEquals(100, hero.getHp());
+
+        hero.setHp(0);
+        assertEquals(0, hero.getHp());
+        hero.setHp(1);
+        assertEquals(1, hero.getHp());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            hero.setHp(-100);
+        });
+
+    }
+}
