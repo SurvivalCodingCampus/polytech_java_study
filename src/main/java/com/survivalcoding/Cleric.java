c049ad59be2079bb2c33755c19916c701c092ff9
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-06-17 09:38:15 +0900

    feat: 클레릭 클래스의 maxHp,maxMp 를 static 으로 선언

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -6,8 +6,8 @@ public class Cleric {
     String name;
     int hp = 50;
     int mp = 10;
-    final int maxHp = 50;
-    final int maxMp = 10;
+    static final int maxHp = 50;
+    static final int maxMp = 10;
 
     // 난수 생성을 위한 Random 객체 한번만 만들자
     Random rand = new Random();
