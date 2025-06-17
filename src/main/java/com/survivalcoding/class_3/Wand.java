ac3078b599ff1d58219ee0bb6e8ca6c2beba2b05
Hae1Won1 <h121581@naver.com>
2025-06-17 14:38:00 +0900

    feat: Wand class 작성 및 테스트

---
+++ src/main/java/com/survivalcoding/class_3/Wand.java
@@ -0,0 +1,33 @@
+package com.survivalcoding.class_3;
+
+public class Wand {
+    String name; // 지팡이의 이름
+    double power; // 지팡이의 마력
+
+    // getter
+    public String getName(){
+        return name;
+    }
+    public double getPower(){
+        return power;
+    }
+
+    //setter
+    public void setName(String name){
+        if(name==null){
+            throw new IllegalArgumentException("이름은 null이 아니어야 함");
+        }
+        if(name.length()<3){
+            throw new IllegalArgumentException("이름은 3문자 이상이어야 함");
+        }
+        this.name = name;
+    }
+
+    public void setPower(double power){
+        if(!(power>=0.5 && power <=100)){
+            throw new IllegalArgumentException("지팡이의 마력은 0.5 이상 100.0 이하여야 함");
+        }
+        this.power = power;
+    }
+
+}
---
+++ src/test/java/com/survivalcoding/class_3/WandTest.java
@@ -0,0 +1,35 @@
+package com.survivalcoding.class_3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WandTest {
+    @Test
+    @DisplayName("Wand getter setter 확인")
+    void getsetWand(){
+        Wand wand = new Wand();
+
+        // get set Name
+        assertThrows(IllegalArgumentException.class,()->{
+            wand.setName("");
+        });
+        assertThrows(IllegalArgumentException.class,()->{
+            wand.setName("ju");
+        });
+        wand.setName("홍길동");
+        assertEquals("홍길동",wand.getName());
+
+        // get set Power
+        assertThrows(IllegalArgumentException.class,()->{
+            wand.setPower(0.2);
+        });
+        assertThrows(IllegalArgumentException.class,()->{
+            wand.setPower(101);
+        });
+        wand.setPower(50);
+        assertEquals(50, wand.getPower());
+    }
+
+}
