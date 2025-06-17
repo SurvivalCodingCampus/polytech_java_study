205220f363575d438253c15f566e354feffb550e
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-06-17 11:56:00 +0900

    feat : Wand class 추가

--- src/main/java/com/survivalcoding/Hero.java
+++ src/main/java/com/survivalcoding/Hero.java
@@ -6,5 +6,15 @@ public class Hero {
     int hp;
     int mp;
 
+    public void setHp(int hp){
+        if(hp<0){
+            throw new IllegalArgumentException("hp는 음수로 들어올 수 없습니다");
+        }
+        this.hp = hp;
+    }
 
+
+    public int getMp() {
+        return mp;
+    }
 }
---
+++ src/main/java/com/survivalcoding/Wand.java
@@ -0,0 +1,7 @@
+package com.survivalcoding;
+
+public class Wand {
+    String name;
+    double power;
+
+}
