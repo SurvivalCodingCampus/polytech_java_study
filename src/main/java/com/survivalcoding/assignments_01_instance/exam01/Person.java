d6f038c9c76ac73d206222bae4e25909e203371a
Jiyun.Jung <modogo3@gmail.com>
2025-06-17 14:39:42 +0900

    타당성 검사 추가

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Person.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Person.java
@@ -2,28 +2,28 @@ package com.survivalcoding.assignments_01_instance.exam01;
 
 public class Person {
     private String name;
-    private int age;
+    private int birthYear;
 
     //이름과 태어난 해를 생성자로 받는다
     //이름과 태어난 해는 한 번 정해지면 수정이 불가
     //getAge()메서드를 통해 나이를 제공하지만, 임의로 수정은 불가
     //나이 계산은 올해년도에서 birthYear 년도를 뺀 값을 리턴한다
         //현재 시간과 날짜는 Date 클래스를 활용하면 얻을 수 있다
-    public Person(String name, int age) {
+    public Person(String name, int birthYear) {
         this.name = name;
-        this.age = age;
+        this.birthYear = birthYear;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getAge() {
-        return age;
+    public int getBirthYear() {
+        return birthYear;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
     }
 
     public static void main(String[] args) {
