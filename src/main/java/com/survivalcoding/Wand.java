ec25fbe345c26ae63a597adb856fa7107d6ccb76
sezondo <psdd205050@gmail.com>
2025-06-17 14:35:07 +0900

    feat : 1번 문제인 마법사 및 지팡이 코드 및 테스트 코드 업로드

---
+++ src/main/java/com/survivalcoding/Wand.java
@@ -0,0 +1,36 @@
+package com.survivalcoding;
+
+public class Wand {
+
+    private String name;
+    private double power;
+
+    Wand(String name, double power){
+        setName(name);
+        setPower(power);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        if (name == null){
+            throw new IllegalArgumentException("해당 이름(지팡이)에 NULL을 집어넣었습니다");
+        }
+        if (3 > name.length()){
+            throw new IllegalArgumentException("이름(지팡이)은 최소 3글자 이상이어야 합니다.");
+        }
+
+        this.name = name;
+    }
+    public double getPower(){
+        return power;
+    }
+    public void setPower(double power){
+        if (power <= 0.5 || power >= 100.0){
+            throw new IllegalArgumentException("지팡이의 power은 0.5이상 100.0 이하여만 합니다");
+        }
+        this.power = power;
+    }
+
+}
---
+++ src/test/java/com/survivalcoding/WandTest.java
@@ -0,0 +1,35 @@
+package com.survivalcoding;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class WandTest {
+
+
+    @Test
+    @DisplayName("완드 생성 테스트")
+    void testWand(){
+        Wand wand = new Wand("지팡이", 50);
+        assertEquals("지팡이", wand.getName());
+        assertEquals(50, wand.getPower());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setName("터짐");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setName(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setPower(0.4);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wand.setPower(100.1);
+        });
+
+    }
+
+
+}
