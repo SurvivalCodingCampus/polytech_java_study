3269cd1a1d1fbf3f3eb316834537ce1157091d5c
seesee1212 <suzumya@naver.com>
2025-06-17 13:37:54 +0900

    Feat: 캡슐화의 정석에따라 접근 지정자를 추가,getter/setter 작성

---
+++ src/main/java/com/survivalcoding/Wand.java
@@ -0,0 +1,23 @@
+package com.survivalcoding;
+
+public class Wand {
+    private String name;   // 지팡이의 이름
+    private double power;  // 지팡이의 마력
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+}
+
--- src/main/java/com/survivalcoding/Wizard.java
+++ src/main/java/com/survivalcoding/Wizard.java
@@ -1,10 +1,46 @@
 package com.survivalcoding;
 
 public class Wizard {
-    String name;
-    int hp;
+    private int hp;
+    private int mp;
+    private String name;
+    private Wand wand;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
 
     void heal(Hero hero) {
-        hero.hp += 10;
+        int basePoint = 10; // 기본회복 포인트
+        int recovPoint = (int)(basePoint * this.wand.getPower()); // 지팡이에 의한 증폭
+        hero.setHp(hero.getHp() + recovPoint); // 용사의 HP를 회복
     }
 }
