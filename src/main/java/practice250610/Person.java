3fb22453894ce125576d3b53c6f31b3dd00eabfb
Dayeong <https://github.com/iiijjjijij>
2025-06-17 14:46:00 +0900

    first commit

---
+++ src/main/java/practice250610/Person.java
@@ -0,0 +1,11 @@
+package practice250610;
+
+public class Person {
+    private String name;
+    private int birthYear;
+
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+}
