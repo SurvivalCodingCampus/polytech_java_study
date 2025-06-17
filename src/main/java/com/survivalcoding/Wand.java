462dd5adc68aad8124c38593666a0fa0706c4500
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-06-17 13:13:36 +0900

    feat : Wand class 타당성 검사 추가

--- src/main/java/com/survivalcoding/Wand.java
+++ src/main/java/com/survivalcoding/Wand.java
@@ -10,6 +10,14 @@ public class Wand {
 
     public void setName(String name) {
         this.name = name;
+
+        if(name == null){
+            throw new IllegalArgumentException("지팡이의 이름은 null일 수 없습니다.");
+        }
+
+        if(name.length()<=3){
+            thorw new IllegalArgumentException("이름이 너무 짧습니다.");
+        }
     }
 
     public double getPower() {
@@ -18,5 +26,13 @@ public class Wand {
 
     public void setPower(double power) {
         this.power = power;
+
+        if(power<0.5){
+            throw new IllegalArgumentException("마력은 0.5이하일 수 없습니다.");
+        }
+
+        if(power>100.0){
+            throw new IllegalArgumentException("마력은 100.0 이하이어야 합니다.");
+        }
     }
 }
