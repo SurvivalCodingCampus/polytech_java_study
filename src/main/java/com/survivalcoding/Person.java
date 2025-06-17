466e1d3831ef9f0a7a1cd23004ed29e052023b6c
Kimjongyun <zla0729@gmail.com>
2025-06-17 16:28:24 +0900

    feat: Person 클래스와 그에 대한 UML 추가

---
+++ src/main/java/com/survivalcoding/Person.java
@@ -0,0 +1,26 @@
+package com.survivalcoding;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Person {
+    private final String name;
+    private final int birthYear;
+
+    Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int currentYear = calendar.get(Calendar.YEAR);
+
+        return (currentYear - birthYear + 1);
+    }
+}
