a82dc2271b1b6fc92533689cf18cd5691a6c2b35
Dayeong <https://github.com/iiijjjijij>
2025-06-17 14:58:33 +0900

    feat: getName(), getAge() 추가

--- src/main/java/practice250610/Person.java
+++ src/main/java/practice250610/Person.java
@@ -1,5 +1,8 @@
 package practice250610;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Person {
     private String name;
     private int birthYear;
@@ -7,5 +10,13 @@ public class Person {
     public Person(String name, int birthYear) {
         this.name = name;
         this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return LocalDate.now().getYear() - birthYear;
     }
 }
