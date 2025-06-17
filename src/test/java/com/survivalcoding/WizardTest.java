61c482939117e6fb03c1391c47b9897136df54e4
morningB <yjw001205@gmail.com>
2025-06-17 13:53:49 +0900

    test: Wizard Test 추가

--- src/test/java/com/survivalcoding/WizardTest.java
+++ src/test/java/com/survivalcoding/WizardTest.java
@@ -2,9 +2,40 @@ package com.survivalcoding;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class WizardTest {
 
     @Test
-    void getHp() {
+    void constructor() {
+        Wand wand = new Wand("마법사의 불꽃", 99);
+        Wizard wizard = new Wizard(1, 2, "3333", wand);
+
+        assertEquals(1, wizard.getHp());
+        assertEquals(2, wizard.getMp());
+        assertEquals("3333", wizard.getName());
+        assertEquals("마법사의 불꽃", wizard.getWand().getName());
+        assertEquals(99, wizard.getWand().getPower());
     }
+
+    @Test
+    void setTester() {
+        Wand wand = new Wand("마법사의 불꽃", 99);
+        Wizard wizard = new Wizard(1, 2, "3333", wand);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            wizard.setHp(-1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wizard.setMp(-1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wizard.setName("o");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            wizard.setWand(null);
+        });
+    }
+
 }
