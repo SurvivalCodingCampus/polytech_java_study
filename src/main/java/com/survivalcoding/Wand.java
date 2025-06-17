3ff48fc692bb4efbe9142f25ee70fc0221f4b775
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-06-17 12:06:51 +0900

    feat : Wand class에 getter/setter 추가

--- src/main/java/com/survivalcoding/Wand.java
+++ src/main/java/com/survivalcoding/Wand.java
@@ -1,7 +1,22 @@
 package com.survivalcoding;
 
 public class Wand {
-    String name;
-    double power;
+    public String name;
+    public double power;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
