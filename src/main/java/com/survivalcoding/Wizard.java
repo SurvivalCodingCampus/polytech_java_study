37042bc66ce4e4d903a7bc2e16c69274245ecc14
Soso-Kiki <polytechid10@gmail.com>
2025-06-23 17:19:38 +0900

    feat: 클래스 'Wizard' 코드 작성

--- src/main/java/com/survivalcoding/Wizard.java
+++ src/main/java/com/survivalcoding/Wizard.java
@@ -1,18 +1,25 @@
 package com.survivalcoding;
 
 public class Wizard {
-    
+
     private int hp;
-    private int mp;
+    private int mp = 100; // 문제 04) 초기값 100으로 설정
     private String name;
     private Wand wand;
 
 
-    public Wizard(String name, int hp, int mp) { // 생성자
-        this.name = name;
-        this.hp = hp;
-        this.mp = mp;
-        this.wand = getWand();
+    public Wizard(String name, int hp, int mp, Wand wand) { // 생성자
+        setName(name);  // 검증 로직 실행
+        setHp(hp);      // 검증 로직 실행
+        setMp(mp);
+        this.wand = wand;
+        // 검증 로직 실행
+//        this.name = name; // 직접 할당
+//        this.hp = hp;
+//        this.mp = mp;
+
+        //this.wand = getWand(); // wand가 null 이면 null 반환하게 됨
+        //wand는 나중에 setWand()로 설정
     }
 
     // HP 관련 get, set
@@ -71,10 +78,26 @@ public class Wizard {
     }
 
     public void heal(Hero hero) {
+        //1. 지팡이 파워로 힐을 실행
         int basePoint = 10; // 기본 회복 포인트
         int recoverPoint = (int) (basePoint * this.wand.getPower()); // 지팡이에 의한 증폭
         //private인 power의 값을 읽기 위해 설정 되어 있는 getPower()로 접근
+
+        //2. 그 다음 MP 체크
         hero.setHp(hero.getHp() + recoverPoint); // 용사의 HP를 회복
+
+        if (this.mp < 10) { // 힐 스킬 사용 mp = 10 이기에 10 이하면 사용x
+            System.out.println("마나가 부족합니다");
+            return;
+        }
+
+        // 3. 다시 힐 실행
+        //힐 : Hero의 hp를 20 회복, 자신의 mp를 10 소모
+        hero.setHp(hero.getHp() + 20);
+        this.setMp(this.getMp() - 10);
+
+        //힐 성공 메세지 출력
+        System.out.println("힐을 시전했습니다. 대상 HP : " + hero.getHp());
 
     }
 
