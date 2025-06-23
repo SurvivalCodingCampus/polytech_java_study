109dcb60f504d6853c76cd67c008aa72849f0818
namyejin <namye6950@gmail.com>
2025-06-23 14:33:37 +0900

    feat : 슬라임 class와 슬라임 test code 추가

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Slime.java
@@ -0,0 +1,31 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+public class Slime {
+    final String suffix;
+    int hp;
+
+    public Slime(int hp, String suffix) {
+        this.hp = hp;
+        this.suffix = suffix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    void attack(Hero hero){
+        System.out.println("슬라임 " + suffix + "이/가 공격했다");
+        System.out.println("10의 데미지");
+
+        hero.setHp(hero.getHp() -10);
+    }
+
+}
---
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/SlimeTest.java
@@ -0,0 +1,26 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlimeTest {
+
+    Slime slime = new Slime(10, "슬라임");
+
+    @Test
+    void getSuffix() {
+        assertEquals("슬라임", slime.getSuffix());
+    }
+
+    @Test
+    void getHp() {
+        assertEquals(10, slime.getHp());
+    }
+
+    @Test
+    void setUP(){
+        slime.setHp(10);
+    }
+
+}
