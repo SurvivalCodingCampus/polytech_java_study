a0fa1177e764ddc8baa8b2e42566986cd372d2ca
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-06-23 10:07:34 +0900

    feat: 캡슐화 Person 클래스

---
+++ src/test/java/com/survivalcoding/PersonTest.java
@@ -0,0 +1,16 @@
+package com.survivalcoding;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    @DisplayName("2000년 생은 25살이다")
+    void getAge() {
+        Person person = new Person("홍길동", 2000);
+        assertEquals(25, person.getAge());
+    }
+}
