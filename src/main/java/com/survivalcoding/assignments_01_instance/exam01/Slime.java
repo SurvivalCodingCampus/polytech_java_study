3b309ff0ae0a19acc55233719e94654beeedd26d
Kangchanghwan <lgodl1598@naver.com>
2025-06-23 13:21:01 +0900

    fix: 슬라임 객체 수정

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Slime.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Slime.java
@@ -2,13 +2,41 @@ package com.survivalcoding.assignments_01_instance.exam01;
 
 public class Slime {
 
-    private String suffix;
+    private final String suffix;
+    private int hp;
+
+
+    public Slime(String suffix) {
+        if(suffix == null || suffix.trim().isEmpty()) {
+            throw new IllegalArgumentException("suffix cannot be null or empty");
+        }
+        this.suffix = suffix;
+    }
+
+    public Slime(String suffix, int hp) {
+        this(suffix);
+        this.hp = hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public int getHp() {
+        return hp;
+    }
 
     public void attack(Attackable attackable){
-        System.out.println("슬라임 " + suffix + "이/가 공격했다");
-        System.out.println("10의 데미지");
+        int attackAmount = 10;
 
-        attackable.takeDamage(10);
+        System.out.println("슬라임 " + suffix + "이/가 공격했다");
+        System.out.println( attackAmount + "의 데미지");
+
+        attackable.takeDamage(attackAmount);
     }
 
 }
