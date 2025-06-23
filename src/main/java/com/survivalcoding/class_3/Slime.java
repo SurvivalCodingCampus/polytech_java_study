7c41b525eef2857ebca3bdffb94cc6709419f7a4
Hae1Won1 <h121581@naver.com>
2025-06-23 15:06:58 +0900

    feat: Slime 클래스 작성

---
+++ src/main/java/com/survivalcoding/class_3/Slime.java
@@ -0,0 +1,38 @@
+package com.survivalcoding.class_3;
+
+public class Slime {
+    // final : 생성자로 생성 후 수정하지 않겠다 선언
+    final String suffix;
+    int hp;
+
+    // 생성자, getter, setter 작성
+    Slime(String suffix, int hp) {
+        this.suffix = suffix;
+        this.hp = hp;
+    }
+
+    Slime(String suffix) {
+        this(suffix, 10);
+    }
+
+    void attack(Hero hero) {
+        System.out.println("슬라임 " + suffix + "이/가 공격했다");
+        System.out.println("10의 데미지");
+
+        hero.setHp(hero.getHp() - 10);
+    }
+
+    //getter
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    //setter
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
