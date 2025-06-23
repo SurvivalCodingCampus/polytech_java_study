a283970bc49712ba524c8eb5babd5612c5e90b22
MJ37031 <rlaalswl0520@naver.com>
2025-06-23 13:27:31 +0900

    feat: poisonCount(초기값 5)-아무나 수정 금지

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/PoisonSlime.java
@@ -0,0 +1,12 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+public class PoisonSlime extends Slime {
+
+    private final int poisonCount = 5;
+
+    public PoisonSlime(String suffix) {
+        super(suffix);
+    }
+
+
+}
--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Wizard.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Wizard.java
@@ -18,9 +18,11 @@ public class Wizard {
     }
 
     public void setHp(int hp) {
+        this.hp = hp;
+
         if (hp < 0) {
             this.hp = 0;
-        } else this.hp = hp;
+        }
     }
 
     public int getMp() {
