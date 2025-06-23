cae33ee27bbaafa205877531d6377d8223692721
Dayeong <https://github.com/iiijjjijij>
2025-06-23 16:50:17 +0900

    feat: 생성자 수정 및 추가

--- src/main/java/practice250610/GreatWizard.java
+++ src/main/java/practice250610/GreatWizard.java
@@ -1,8 +1,9 @@
 package practice250610;
 
 public class GreatWizard extends Wizard {
+    //mp = 150
     public GreatWizard() {
-        setMp(150);
+        super("Gwizard", 100, 150);
     }
 
     @Override
@@ -20,13 +21,12 @@ public class GreatWizard extends Wizard {
     }
 
     public void superHeal(Hero hero) {
-        int basePoint = 100; // 기본 회복 포인트
         int mpPoint = 50;
 
         if (getMp() < mpPoint) {
             System.out.println("마나가 부족합니다");
         } else {
-            hero.setHp(basePoint);
+            hero.setHp(hero.getMaxHp());
             setMp(getMp() - mpPoint);
             System.out.println("슈퍼 힐을 시전했습니다. " + hero.getName() + " HP: " + hero.getHp());
         }
