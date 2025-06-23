82ee29ed30afbd3bae2abe9fb40fa15ea7378a29
seesee1212 <suzumya@naver.com>
2025-06-23 16:46:10 +0900

    Feat: Slime 수정

--- src/main/java/com/survivalcoding/Slime.java
+++ src/main/java/com/survivalcoding/Slime.java
@@ -2,9 +2,11 @@ package com.survivalcoding;
 
 public class Slime {
     protected final String suffix;
+    private int hp;
 
     public Slime(String suffix) {
         this.suffix = suffix;
+        this.hp = 100;
     }
 
     public void attack(Hero hero) {
@@ -12,4 +14,13 @@ public class Slime {
         System.out.println("10의 데미지");
         hero.setHp(hero.getHp() - 10);
     }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = Math.max(hp, 0); // 음수 방지
+    }
 }
+
