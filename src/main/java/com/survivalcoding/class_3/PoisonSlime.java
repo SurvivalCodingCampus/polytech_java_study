5689448e7787671f17c62bb10c0a48a8456f0d14
Hae1Won1 <h121581@naver.com>
2025-06-23 15:17:40 +0900

    feat: PoisonSlime 클래스 및 테스트 작성

--- src/main/java/com/survivalcoding/class_3/PoisonSlime.java
+++ src/main/java/com/survivalcoding/class_3/PoisonSlime.java
@@ -14,7 +14,7 @@ public class PoisonSlime extends Slime {
         poisonCount = setPoison;
     }
 
-    void attack(Hero hero) {
+    public void attack(Hero hero) {
         super.attack(hero);
         if (poisonCount != 0) {
             System.out.println("추가로, 독 포자를 살표했다!");
