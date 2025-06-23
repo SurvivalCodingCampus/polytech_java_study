1b126b9475a51cbe7c771b9faf6b085698951e0e
MJ37031 <rlaalswl0520@naver.com>
2025-06-23 15:08:39 +0900

    feat: PoisonSlime attack()

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/PoisonSlime.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/PoisonSlime.java
@@ -2,11 +2,40 @@ package com.survivalcoding.assignments_01_instance.exam01;
 
 public class PoisonSlime extends Slime {
 
-    private final int poisonCount = 5;
+    private int poisonCount = 5;
 
     public PoisonSlime(String suffix) {
         super(suffix);
     }
 
+    public int getPoisonCount() {
+        return poisonCount;
+    }
 
+    private void setPoisonCount(int poisonCount) {
+        this.poisonCount = poisonCount;
+    }
+
+    @Override
+    void attack(Hero hero) {
+        super.attack(hero);
+
+        if (getPoisonCount() != 0) {
+            System.out.println("추가로, 독 포자를 살포했다 !");
+
+            int minushp = hero.getHp() - hero.getHp() / 5;
+            hero.setHp(minushp);
+            System.out.println(minushp + " 포인트의 데미지");
+            setPoisonCount(getPoisonCount() - 1);  // poisoncount 1 감소
+
+        }
+    }
+
+    public static void main(String[] args) {
+        PoisonSlime poisonSlime = new PoisonSlime("A");
+        Hero hero = new Hero("용사", 50);
+
+        poisonSlime.attack(hero);
+
+    }
 }
---
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/PoisonSlimeTest.java
@@ -0,0 +1,24 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PoisonSlimeTest {
+    @Test
+    @DisplayName("hero의 hp를 20 회복시키고 자신의 mp를 10 소모")
+    void testPoisonSlimeattack() {
+        //given
+        PoisonSlime poisonSlime = new PoisonSlime("A");
+        Hero hero = new Hero("Hero", 100);
+
+        poisonSlime.attack(hero);
+
+        // then
+        assertEquals(72, hero.getHp());
+        assertEquals(4, poisonSlime.getPoisonCount());
+
+    }
+
+}
