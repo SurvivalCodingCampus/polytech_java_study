109e08cd11c5e4bd077b57f75150db7c235a56fe
MJ37031 <rlaalswl0520@naver.com>
2025-06-23 15:45:13 +0900

    수업내용

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Hero.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Hero.java
@@ -4,11 +4,40 @@ import java.util.Random;
 
 public class Hero {
     static int money = 100; //공공자원
-    String name;
-    int hp;
-    Sword sword;
+    private int max_hp = 100;
+    private String name;
+    private int hp;
+    private Sword sword;
 
-    Hero() {
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMax_hp() {
+        return max_hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Sword getSword() {
+        return sword;
+    }
+
+    public void setSword(Sword sword) {
+        this.sword = sword;
+    }
+
+    public Hero() {
         this.hp = 100;
         this.name = "김영웅";
     }
@@ -27,5 +56,7 @@ public class Hero {
         money = new Random().nextInt(1000);
     }
 
-
+    public void run() {
+        System.out.println("run");
+    }
 }
