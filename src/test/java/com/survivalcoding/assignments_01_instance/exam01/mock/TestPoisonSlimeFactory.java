0889412cfe669f7c2c65fa21a5112e5d524c8593
Kangchanghwan <lgodl1598@naver.com>
2025-06-23 14:20:16 +0900

    fix: test 코드 수정

---
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/mock/TestPoisonSlimeFactory.java
@@ -0,0 +1,12 @@
+package com.survivalcoding.assignments_01_instance.exam01.mock;
+
+import com.survivalcoding.assignments_01_instance.exam01.PoisonSlime;
+
+public class TestPoisonSlime {
+
+
+    public static PoisonSlime createZeroCountSlime(){
+        return new PoisonSlime("test",100, 0);
+    }
+
+}
