bb4cb58e1b9b5b51ce1de685b6d16761c325e378
namyejin <namye6950@gmail.com>
2025-06-24 13:17:18 +0900

    feat : TangibleAsset 추상 클래스, Book 클래스, Computer 클래스 추가

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Book.java
@@ -0,0 +1,9 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+public class Book extends TangibleAsset {
+
+    private String name;
+    private int price;
+    private String color;
+    private String isbn;
+}
---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Computer.java
@@ -0,0 +1,11 @@
+package com.survivalcoding.assignments_01_instance.exam01;
+
+import java.util.stream.StreamSupport;
+
+public class Computer extends TangibleAsset {
+
+    private String name;
+    private int price;
+    private String color;
+    private String makerName;
+}
