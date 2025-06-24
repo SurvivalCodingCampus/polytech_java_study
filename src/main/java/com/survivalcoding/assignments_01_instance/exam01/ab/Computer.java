cc9def6462996ea6133fe82a6f0d71e52a217f1c
Jiyun.Jung <modogo3@gmail.com>
2025-06-24 13:29:28 +0900

    class 추가

---
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/ab/Computer.java
@@ -0,0 +1,10 @@
+package com.survivalcoding.assignments_01_instance.exam01.ab;
+
+//부모 클래스를 활용한 형태로 수정
+
+public class Computer {
+    String name;
+    int price;
+    String color;
+    String makerName;
+}
