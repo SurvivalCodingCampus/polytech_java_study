4bf0d17613eeb3ce2e0e23f541401ac5449c6972
morningB <yjw001205@gmail.com>
2025-06-24 13:23:54 +0900

    feat: book class 캡슐화 진행, 생성자 추가

--- src/main/java/com/survivalcoding/six/Book.java
+++ src/main/java/com/survivalcoding/six/Book.java
@@ -1,5 +1,21 @@
 package com.survivalcoding.six;
 
 public class Book extends TangibleAsset {
-    String isbn;
+    private String isbn;
+
+    public Book(String name, int price, String color, double weight, String isbn) {
+        super(name, price, color, weight);
+        setIsbn(isbn);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        if (isbn == null) {
+            throw new NullPointerException("Isbn은 null일 수 없다.");
+        }
+        this.isbn = isbn;
+    }
 }
