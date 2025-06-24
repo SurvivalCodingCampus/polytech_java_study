96490c57e6b976297403b984d3988a71358b2cf4
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-06-24 13:14:19 +0900

    feat : TangibleAsset abstract class 추가

---
+++ src/main/java/com/survivalcoding/Asset/TangibleAsset.java
@@ -0,0 +1,31 @@
+package com.survivalcoding.Asset;
+
+public abstract class TangibleAsset {
+    private String name;
+    private int price;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
