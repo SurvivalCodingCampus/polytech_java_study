82d1b248c67c15a7946b0002008e7b5ea2f73102
Dayeong <https://github.com/iiijjjijij>
2025-06-24 10:45:28 +0900

    feat: superHealPoint 필드 추가, 추가한 필드로 메서드 수정

--- src/main/java/practice250610/GreatWizard.java
+++ src/main/java/practice250610/GreatWizard.java
@@ -1,34 +1,36 @@
 package practice250610;
 
 public class GreatWizard extends Wizard {
+    private int superMpPoint;
+
     //mp = 150
     public GreatWizard() {
         super("Gwizard", 100, 150);
+        setSuperMpPoint(50);
+        setHealPoint(25);
+        setMpPoint(5);
     }
 
     @Override
     public void heal(Hero hero) {
-        int basePoint = 25; // 기본 회복 포인트
-        int mpPoint = 5;
-
-        if (getMp() < mpPoint) {
-            System.out.println("마나가 부족합니다");
-        } else {
-            hero.setHp(hero.getHp() + basePoint);
-            setMp(getMp() - mpPoint);
-            System.out.println("힐을 시전했습니다. " + hero.getName() + " HP: " + hero.getHp());
-        }
+        super.heal(hero);
     }
 
     public void superHeal(Hero hero) {
-        int mpPoint = 50;
-
-        if (getMp() < mpPoint) {
+        if (getMp() < getSuperMpPoint()) {
             System.out.println("마나가 부족합니다");
         } else {
             hero.setHp(hero.getMaxHp());
-            setMp(getMp() - mpPoint);
+            setMp(getMp() - getSuperMpPoint());
             System.out.println("슈퍼 힐을 시전했습니다. " + hero.getName() + " HP: " + hero.getHp());
         }
+    }
+
+    public int getSuperMpPoint() {
+        return superMpPoint;
+    }
+
+    public void setSuperMpPoint(int superMpPoint) {
+        this.superMpPoint = superMpPoint;
     }
 }
