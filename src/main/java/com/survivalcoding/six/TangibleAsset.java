9d0e4757da2a416c8ff075ceacb7e0543c580117
morningB <yjw001205@gmail.com>
2025-06-24 13:06:38 +0900

    feat: Thing 인터페이스 implements 후 오버라이딩

--- src/main/java/com/survivalcoding/six/TangibleAsset.java
+++ src/main/java/com/survivalcoding/six/TangibleAsset.java
@@ -1,6 +1,17 @@
 package com.survivalcoding.six;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
     String color;
+    double weight;
 
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
 }
