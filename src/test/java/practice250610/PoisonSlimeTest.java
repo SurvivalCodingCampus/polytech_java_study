4c51b5304f371a382e0f1b0a1149925b53e7dab8
Dayeong <https://github.com/iiijjjijij>
2025-06-24 10:16:56 +0900

    test: 데미지 10을 getDamage()로 수정

--- src/test/java/practice250610/PoisonSlimeTest.java
+++ src/test/java/practice250610/PoisonSlimeTest.java
@@ -22,10 +22,11 @@ class PoisonSlimeTest {
         Hero hero = new Hero();
         PoisonSlime poisonSlime = new PoisonSlime("A");
         int damage = 0, currentHp = 500, poison = 5;
+        int baseDamage = poisonSlime.getDamage();
 
         hero.setHp(currentHp);
         poisonSlime.attack(hero);
-        damage = 10 + ((currentHp - 10) / 5);
+        damage = baseDamage + ((currentHp - baseDamage) / 5);
         currentHp -= damage;
         poison--;
 
@@ -40,16 +41,17 @@ class PoisonSlimeTest {
         Hero hero = new Hero();
         PoisonSlime poisonSlime = new PoisonSlime("A");
         int damage, currentHp = 500, poison = 5;
+        int baseDamage = poisonSlime.getDamage();
 
         hero.setHp(currentHp);
         for (int i = poison; poison > 0; poison--) {
             poisonSlime.attack(hero);
-            damage = 10 + ((currentHp - 10) / 5);
+            damage = baseDamage + ((currentHp - baseDamage) / 5);
             currentHp -= damage;
         }
 
         poisonSlime.attack(hero);
-        currentHp -= 10;
+        currentHp -= baseDamage;
 
         assertEquals(currentHp, hero.getHp());
         assertEquals(poison, poisonSlime.getPoisonCount());
