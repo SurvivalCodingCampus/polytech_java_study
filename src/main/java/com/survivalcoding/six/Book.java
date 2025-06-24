ffb7bcf63d50a382b512aa960a3130828447c94e
morningB <yjw001205@gmail.com>
2025-06-24 12:28:40 +0900

    feat: tangible을 상속받는 유형자산 추가

---
+++ src/main/java/com/survivalcoding/six/Book.java
@@ -0,0 +1,5 @@
+package com.survivalcoding.six;
+
+public class Book extends TangibleAsset {
+    String isbn;
+}
---
+++ src/main/java/com/survivalcoding/six/Computer.java
@@ -0,0 +1,5 @@
+package com.survivalcoding.six;
+
+public class Computer extends TangibleAsset {
+    String makerName;
+}
