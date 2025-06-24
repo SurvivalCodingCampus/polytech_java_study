642eb4d28e8f88ca12a9b49e24fb31225d9ba861
Dayeong <https://github.com/iiijjjijij>
2025-06-24 15:00:19 +0900

    first commit

---
+++ src/test/java/Asset/BookTest.java
@@ -0,0 +1,18 @@
+package Asset;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+    Book book = new Book();
+
+    @Test
+    @DisplayName("Asset 클래스의 name 출력 확인")
+    void name() {
+        String input = "종이컵";
+        book.setName(input);
+        assertEquals(input, book.getName());
+    }
+}
