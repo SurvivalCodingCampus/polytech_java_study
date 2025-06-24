c371e20b9687fe9ebd94578438f8e7d82696b60d
morningB <yjw001205@gmail.com>
2025-06-24 13:24:15 +0900

    feat: tangibleAsset class 캡슐화 진행, 생성자 추가

--- src/main/java/com/survivalcoding/six/TangibleAsset.java
+++ src/main/java/com/survivalcoding/six/TangibleAsset.java
@@ -1,8 +1,25 @@
 package com.survivalcoding.six;
 
 public abstract class TangibleAsset extends Asset implements Thing {
-    String color;
-    double weight;
+    private String color;
+    private double weight;
+
+    public TangibleAsset(String name, int price, String color, double weight) {
+        super(name, price);
+        setColor(color);
+        setWeight(weight);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null) {
+            throw new IllegalArgumentException("color는 null일 수 없다.");
+        }
+        this.color = color;
+    }
 
     @Override
     public double getWeight() {
@@ -11,7 +28,10 @@ public abstract class TangibleAsset extends Asset implements Thing {
 
     @Override
     public void setWeight(double weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("무게는 음수일 수 없다.");
+        }
         this.weight = weight;
     }
-    
+
 }
