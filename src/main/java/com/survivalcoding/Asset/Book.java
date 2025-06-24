cf66b46791f9aa04d934520ae33a2c7b6af2764f
Soso-Kiki <polytechid10@gmail.com>
2025-06-24 15:32:55 +0900

    feat: 클래스 'Book' 작성

---
+++ src/main/java/com/survivalcoding/Asset/Book.java
@@ -0,0 +1,23 @@
+package com.survivalcoding.Asset;
+
+public class Book extends TangibleAsset {
+
+
+    private String isbn;
+
+    public Book(String name, int price, String color, double weight, String isbn) {
+        super(name, price, color, weight);
+        this.isbn = isbn;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+}
