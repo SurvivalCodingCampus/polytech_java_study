5450191e7f3bb1066e1467531337067938bf25a5
Hae1Won1 <h121581@naver.com>
2025-06-24 13:53:09 +0900

    fix: Thing 인터페이스 수정

--- src/main/java/com/survivalcoding/class_3/TangibleAsset.java
+++ src/main/java/com/survivalcoding/class_3/TangibleAsset.java
@@ -1,6 +1,6 @@
 package com.survivalcoding.class_3;
 
-public abstract class TangibleAsset extends Asset implements thing{
+public abstract class TangibleAsset extends Asset implements Thing {
     String color;
     double weight;
 }
