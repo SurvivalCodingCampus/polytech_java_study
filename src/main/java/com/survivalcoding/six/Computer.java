45e8d71088913887296d9ac6e2fdada4191e8b0e
morningB <yjw001205@gmail.com>
2025-06-24 13:24:04 +0900

    feat: computer class 캡슐화 진행, 생성자 추가

--- src/main/java/com/survivalcoding/six/Computer.java
+++ src/main/java/com/survivalcoding/six/Computer.java
@@ -1,5 +1,21 @@
 package com.survivalcoding.six;
 
 public class Computer extends TangibleAsset {
-    String makerName;
+    private String makerName;
+
+    public Computer(String name, int price, String color, double weight) {
+        super(name, price, color, weight);
+        setMakerName(color);
+    }
+
+    public String getMakerName() {
+        return makerName;
+    }
+
+    public void setMakerName(String makerName) {
+        if (makerName == null) {
+            throw new IllegalArgumentException("MakerName은 null일 수 없다.");
+        }
+        this.makerName = makerName;
+    }
 }
