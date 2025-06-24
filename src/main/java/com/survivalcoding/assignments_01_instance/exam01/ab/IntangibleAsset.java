4d7c4cb5a070bed539af01bc8d153be78786c9e5
Jiyun.Jung <modogo3@gmail.com>
2025-06-24 14:27:21 +0900

    Asset.puml 추가

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/ab/IntangibleAsset.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/ab/IntangibleAsset.java
@@ -2,5 +2,5 @@ package com.survivalcoding.assignments_01_instance.exam01.ab;
 
 //자산 (Asset)이 부모 클래스
 
-public class intangibleAsset extends Asset {
+public class IntangibleAsset extends Asset {
 }
--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/ab/Patent.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/ab/Patent.java
@@ -2,5 +2,5 @@ package com.survivalcoding.assignments_01_instance.exam01.ab;
 
 //무형자산(intangibleAsset)이 부모 클래스
 
-public class Patent extends intangibleAsset{
+public class Patent extends IntangibleAsset {
 }
