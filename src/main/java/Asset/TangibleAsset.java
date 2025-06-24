404f07c48f639e119390b344c867765443acbb57
Dayeong <https://github.com/iiijjjijij>
2025-06-24 13:59:39 +0900

    feat: Thing 인터페이스 상속, weight 필드 추가

--- src/main/java/Asset/TangibleAsset.java
+++ src/main/java/Asset/TangibleAsset.java
@@ -1,7 +1,8 @@
 package Asset;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
     private String color;
+    private int weight;
 
     public String getColor() {
         return this.color;
@@ -9,5 +10,15 @@ public abstract class TangibleAsset extends Asset {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    @Override
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
