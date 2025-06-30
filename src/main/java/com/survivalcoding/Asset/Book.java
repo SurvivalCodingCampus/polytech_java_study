3ea20dd0a525c5aa2110d4fd8d22e50283967fb0
Soso-Kiki <polytechid10@gmail.com>
2025-06-30 09:31:53 +0900

    fix: 주석 수정

--- src/main/java/com/survivalcoding/Asset/Book.java
+++ src/main/java/com/survivalcoding/Asset/Book.java
@@ -3,7 +3,7 @@ package com.survivalcoding.Asset;
 public class Book extends TangibleAsset {
 
 
-    private String isbn;
+    private String isbn; // 책 고유 번호
 
     public Book(String name, int price, String color, double weight, String isbn) {
         super(name, price, color, weight);
