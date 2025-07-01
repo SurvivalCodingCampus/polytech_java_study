5248c595b7f79c470f13e3f40799685cda0212c8
MJ37031 <rlaalswl0520@naver.com>
2025-07-01 13:44:16 +0900

    feat: 연습문제 4-2 열쇠의 종류 나타내는 열거형 KeyType 정의

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBox.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBox.java
@@ -2,12 +2,27 @@ package com.survivalcoding.assignments_01_instance.exam01.generic;
 
 public class StrongBox<E> {
     private E data;
+    private int count = 0;
+
+    public enum KeyType {
+        PADLOCK,
+        BUTTON,
+        DIAL,
+        FINGER
+    }
+
+    public StrongBox(KeyType keyType) {
+        this.keyType = keyType;
+    }
+
+    KeyType keyType = KeyType.BUTTON;
 
     public void put(E data) {
         this.data = data;
     }
 
     public E get() {
+        count++;
         return this.data;
     }
 }
