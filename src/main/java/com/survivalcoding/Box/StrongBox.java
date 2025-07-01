cbd1bc9a85486c05de3c215ec32247cd0c65f60f
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-07-01 14:46:35 +0900

    feat : 과제 2 작성중

--- src/main/java/com/survivalcoding/Box/StrongBox.java
+++ src/main/java/com/survivalcoding/Box/StrongBox.java
@@ -2,12 +2,57 @@ package com.survivalcoding.Box;
 
 public class StrongBox<E> {
     private E data;
+    private KeyType keyType;
+    private int count = 0;
 
     public void put(E data) {
         this.data = data;
     }
 
+    enum KeyType {
+        PADLOCK(1024),
+        BUTTON(10000),
+        DIAL(30000),
+        FINGER(1000000);
+
+        private int value;
+
+        KeyType(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    StrongBox(KeyType keyType) {
+        this.keyType = keyType;
+    }
+
     public E get() {
+        this.count++;
+        switch (keyType) {
+            case PADLOCK:
+                System.out.println("PADLOCK");
+                break;
+            case BUTTON:
+                System.out.println("BUTTON");
+                break;
+            case DIAL:
+                System.out.println("DIAL");
+                break;
+            case FINGER:
+                System.out.println("FINGER");
+                break;
+            default:
+                return null;
+        }
+
         return this.data;
     }
+
+
 }
+
+
