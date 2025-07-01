39b66396c0f1ebdfd03d8beb3115263f3f8b910c
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-07-01 15:45:12 +0900

    feat :  test code 나머지 추가

--- src/test/java/com/survivalcoding/Box/StrongBoxTest.java
+++ src/test/java/com/survivalcoding/Box/StrongBoxTest.java
@@ -22,6 +22,20 @@ class StrongBoxTest {
     }
 
     @Test
+    @DisplayName("1024번 이전이면 PADLOCK으로 부터 데이터가 안나오는가??")
+    void StrongBox_1() {
+        //given
+        StrongBox<String> strongBox = new StrongBox<>(KeyType.PADLOCK);
+        strongBox.put("Hi");
+        //when
+        for (int i = 1; i <= 1022; i++) {
+            strongBox.get();
+        }
+        //then
+        assertEquals(null, strongBox.get());
+    }
+
+    @Test
     @DisplayName("10000번째에는 데이터가 나오는가?")
     void strongBox2() {
         //given
@@ -33,6 +47,20 @@ class StrongBoxTest {
         }
         //then
         assertEquals("XYLITOL", strongBox.get());
+    }
+
+    @Test
+    @DisplayName("10000번 이전이면 BUTTON으로 부터 데이터가 안나오는가??")
+    void StrongBox2_2() {
+        //given
+        StrongBox<String> strongBox = new StrongBox<>(KeyType.BUTTON);
+        strongBox.put("Hi");
+        //when
+        for (int i = 1; i <= 9998; i++) {
+            strongBox.get();
+        }
+        //then
+        assertEquals(null, strongBox.get());
     }
 
     @Test
@@ -50,6 +78,21 @@ class StrongBoxTest {
     }
 
     @Test
+    @DisplayName("30000번 이전이면 DIAL으로 부터 데이터가 안나오는가??")
+    void StrongBox3_2() {
+        //given
+        StrongBox<String> strongBox = new StrongBox<>(KeyType.DIAL);
+        strongBox.put("Hi");
+        //when
+        for (int i = 1; i <= 9998; i++) {
+            strongBox.get();
+        }
+        //then
+        assertEquals(null, strongBox.get());
+    }
+
+
+    @Test
     @DisplayName("1000000번째에는 데이터가 나오는가?")
     void strongBox4() {
         //given
@@ -61,6 +104,20 @@ class StrongBoxTest {
         }
         //then
         assertEquals("LOL", strongBox.get());
+    }
+
+    @Test
+    @DisplayName("1,000,000번 이전이면 FINGER으로 부터 데이터가 안나오는가??")
+    void StrongBox4_2() {
+        //given
+        StrongBox<String> strongBox = new StrongBox<>(KeyType.FINGER);
+        strongBox.put("Hi");
+        //when
+        for (int i = 1; i <= 250000; i++) {
+            strongBox.get();
+        }
+        //then
+        assertEquals(null, strongBox.get());
     }
 
 
