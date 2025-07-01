51e51652f35ac0e84130f219ede360d3e8409aac
MJ37031 <rlaalswl0520@naver.com>
2025-07-01 15:28:30 +0900

    feat: 연습문제 4-2 금고에서 호출할 때 횟수 카운트, 한도에 도달하기 전까지는 null 리턴

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBox.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBox.java
@@ -3,8 +3,23 @@ package com.survivalcoding.assignments_01_instance.exam01.generic;
 public class StrongBox<E> {
     private E data;
     private int count = 0;
-    private KeyType keyType;
-    
+    private final KeyType keyType;
+
+    private int getLimitByKeyType() {
+        switch (keyType) {
+            case PADLOCK:
+                return 1024;
+            case BUTTON:
+                return 10000;
+            case DIAL:
+                return 30000;
+            case FINGER:
+                return 1000000;
+            default:
+                throw new IllegalArgumentException("알 수 없음");
+        }
+    }
+
     public StrongBox(KeyType keyType) {
         this.keyType = keyType;
     }
@@ -17,11 +32,15 @@ public class StrongBox<E> {
         this.data = data;
     }
 
+    public int getCount() {
+        return count;
+    }
+
     public E get() {
         count++;
-        if (count < keyType.getLimit()) {
+        if (count <= getLimitByKeyType()) {
             return null;
         }
-        return this.data;
+        return data;
     }
 }
