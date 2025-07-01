00d871ed14fe52a65e64020ce9d909def918ffde
Jiyun.Jung <modogo3@gmail.com>
2025-07-01 16:30:37 +0900

    StrongBoxTest 조건 추가

--- src/test/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBoxTest.java
+++ src/test/java/com/survivalcoding/assignments_01_instance/exam01/generic/StrongBoxTest.java
@@ -2,6 +2,26 @@ package com.survivalcoding.assignments_01_instance.exam01.generic;
 
 //각 KeyType으로 StrongBox를 엶
 
-class StrongBoxTest {
+import com.survivalcoding.assignments_01_instance.exam01.generic.StrongBox;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class StrongBoxTest {
+    @Test
+    @DisplayName("금고 생성")
+    void testStrongBox(){
+
+    }
+
+    @Test
+    @DisplayName("KeyType 임계값 테스트")
+    void KeyTypes(){
+
+    }
+
+    @Test
+    @DisplayName("임계값 도달 후 추가 get() 호출)
+            void testGet(){
+
+    }
 }
