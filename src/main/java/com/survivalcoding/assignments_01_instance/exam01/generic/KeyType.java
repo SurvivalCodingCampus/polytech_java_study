0b411dddd9b4dcc70362873022fa5ce35d9bc1e5
MJ37031 <rlaalswl0520@naver.com>
2025-07-01 15:29:13 +0900

    feat: 열쇠의 종류를 나타내는 열거형 KeyType 정의

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/KeyType.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/generic/KeyType.java
@@ -1,18 +1,8 @@
 package com.survivalcoding.assignments_01_instance.exam01.generic;
 
 public enum KeyType {
-    PADLOCK(1024),
-    BUTTON(10000),
-    DIAL(30000),
-    FINGER(1000000);
-
-    private final int limit;
-
-    KeyType(int limit) {
-        this.limit = limit;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
+    PADLOCK,
+    BUTTON,
+    DIAL,
+    FINGER;
 }
