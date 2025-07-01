107a2920363f7111056b4d3f89972ce119e1a8e0
Dayeong <https://github.com/iiijjjijij>
2025-07-01 15:49:26 +0900

    test: 테스트 코드 추가

--- src/test/java/com/survivalcoding/generic/StrongBoxTest.java
+++ src/test/java/com/survivalcoding/generic/StrongBoxTest.java
@@ -7,23 +7,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StrongBoxTest {
     @Test
-    @DisplayName("String 타입으로 put()/get() 확인")
-    void inputString() {
-        StrongBox<String> instance = new StrongBox<>();
-        String str = "JAVA";
+    @DisplayName("PADLOCK을 넣어 keyType, keyCount 확인")
+    void inputKeyTypePADLOCK() {
+        StrongBox<String> box = new StrongBox<>("GOLD", "PADLOCK");
+        KeyType resKey = KeyType.PADLOCK;
+        int resKeyCount = box.getPadlock();
 
-        instance.put(str);
-        assertEquals(str, instance.getInstance());
+        assertEquals(resKey, box.getKey());
+        assertEquals(resKeyCount, box.getKeyCount());
     }
 
     @Test
-    @DisplayName("double 타입으로 put()/get() 확인")
-    void inputDouble() {
-        StrongBox<Double> instance = new StrongBox<>();
-        Double d = 3.141592;
+    @DisplayName("BUTTON을 넣어 keyType, keyCount 확인")
+    void inputKeyTypeBUTTON() {
+        StrongBox<String> box = new StrongBox<>("GOLD", "BUTTON");
+        KeyType resKey = KeyType.BUTTON;
+        int resKeyCount = box.getButton();
 
-        instance.put(d);
-        assertEquals(d, instance.getInstance());
+        assertEquals(resKey, box.getKey());
+        assertEquals(resKeyCount, box.getKeyCount());
     }
 
+    @Test
+    @DisplayName("DIAL을 넣어 keyType, keyCount 확인")
+    void inputKeyTypeDIAL() {
+        StrongBox<String> box = new StrongBox<>("GOLD", "DIAL");
+        KeyType resKey = KeyType.DIAL;
+        int resKeyCount = box.getDial();
+
+        assertEquals(resKey, box.getKey());
+        assertEquals(resKeyCount, box.getKeyCount());
+    }
+
+    @Test
+    @DisplayName("FINGER을 넣어 keyType, keyCount 확인")
+    void inputKeyTypeFINGER() {
+        StrongBox<String> box = new StrongBox<>("GOLD", "FINGER");
+        KeyType resKey = KeyType.FINGER;
+        int resKeyCount = box.getFinger();
+
+        assertEquals(resKey, box.getKey());
+        assertEquals(resKeyCount, box.getKeyCount());
+    }
+
+    @Test
+    @DisplayName("잘못된 KeyType을 넣었을 시 오류 확인")
+    void inputKeyTypeMiss() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            StrongBox<String> box = new StrongBox<>("GOLD", "Miss");
+        });
+    }
+
+    @Test
+    @DisplayName("get() 호출 시 useKeyCount 1씩 증가")
+    void useKeyCount() {
+        StrongBox<String> box = new StrongBox<>("GOLD", "DIAL");
+        int i = 0;
+        for (i = 0; i < 5; i++) {
+            box.getInstance();
+        }
+
+        assertEquals(box.getUseKeyCount(), i);
+    }
+
+    @Test
+    @DisplayName("get() 호출 시 KeyCount를 모두 쓰면 인스턴스를 얻을 수 있음")
+    void getInstance() {
+        String instance = "GOLD";
+        StrongBox<String> box = new StrongBox<>(instance, "PADLOCK");
+        int top = box.getKeyCount();
+        String res;
+
+        for (int i = 0; i <= top; i++) {
+            box.getInstance();
+        }
+
+        assertEquals(instance, box.getInstance());
+    }
 }
