c18040c44c547bc64bcb7f91193ab30998cbc0b6
seesee1212 <suzumya@naver.com>
2025-07-01 14:13:37 +0900

    Feat:KeyType

--- src/main/java/com/survivalcoding/StrongBox.java
+++ src/main/java/com/survivalcoding/StrongBox.java
@@ -2,11 +2,25 @@ package com.survivalcoding;
 
 public class StrongBox<K> {
     private K item;
+    private final KeyType keyType;
+    private int useCount;
+
+    public StrongBox(KeyType keyType) {
+        this.keyType = keyType;
+        this.useCount = 0;
+    }
+
     public void put(K item) {
         this.item = item;
     }
 
     public K get() {
-        return this.item;
+        useCount++;
+        if (useCount < keyType.getLimit()) {
+            return null;
+        } else {
+            return item;
+        }
     }
 }
+
