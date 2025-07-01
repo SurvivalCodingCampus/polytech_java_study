bd8c2dd8c489285621e587c9d5e8bc8e5da46534
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-07-01 15:33:47 +0900

    feat :  test code 1

--- src/test/java/com/survivalcoding/Box/StrongBoxTest.java
+++ src/test/java/com/survivalcoding/Box/StrongBoxTest.java
@@ -3,15 +3,22 @@ package com.survivalcoding.Box;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class StrongBoxTest {
 
     @Test
-    @DisplayName("1024번째에 PADLOCK 열쇠가 나오는가?")
+    @DisplayName("1024번째에 PADLOCK으로 부터 데이터가 나오는가??")
     void StrongBox() {
         //given
-        StrongBox<String> strongBox =
+        StrongBox<String> strongBox = new StrongBox<>(KeyType.PADLOCK);
+        strongBox.put("Hi");
         //when
-
+        for (int i = 1; i <= 1023; i++) {
+            strongBox.get();
+        }
+        //then
+        assertEquals("Hi", strongBox.get());
     }
 
 }
