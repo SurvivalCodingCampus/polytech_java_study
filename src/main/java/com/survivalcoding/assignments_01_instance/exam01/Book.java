84b8ea412768c0275be703cb7e3aa853e8d3b99c
namyejin <namye6950@gmail.com>
2025-07-07 17:16:57 +0900

    feat: Book class 추가

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Book.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Book.java
@@ -1,18 +1,65 @@
 package com.survivalcoding.assignments_01_instance.exam01;
+import java.time.LocalDate;
+import java.util.*;
 
-public class Book extends TangibleAsset {
+public class Book implements Comparable<Book> {
+    private String title;
+    private Date publishDate;
+    private String comment;
 
-   public Book(String name, int price, String color, double weight){
-       super(name, price, color, weight);
-   }
-
-    private String isbn;
-
-    public String getIsbn() {
-        return isbn;
+    public Book(String title, Date publishDate, String comment) {
+        this.title = title;
+        this.publishDate = publishDate;
+        this.comment = comment;
     }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(publishDate, book.publishDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, publishDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", publishDate=" + publishDate +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return 0;
     }
 }
