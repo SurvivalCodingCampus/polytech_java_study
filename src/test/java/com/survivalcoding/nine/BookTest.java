7ecdf35e79883404e4866f411f6bd5775f970d9a
morningB <yjw001205@gmail.com>
2025-07-07 15:02:15 +0900

    test: 같은 날짜 다른 시간 비교 케이스 추가

--- src/test/java/com/survivalcoding/nine/BookTest.java
+++ src/test/java/com/survivalcoding/nine/BookTest.java
@@ -3,7 +3,7 @@ package com.survivalcoding.nine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,7 +14,7 @@ class BookTest {
     @Test
     @DisplayName("생성자, getter, setter 테스트")
     void testBookCreationAndGetters() {
-        LocalDate publishDate = LocalDate.of(2020, 1, 1);
+        LocalDateTime publishDate = LocalDateTime.now();
 
         Book book = new Book("Title1", publishDate, "Comment1");
 
@@ -26,7 +26,7 @@ class BookTest {
     @Test
     @DisplayName("서로 다른 객체지만 날짜와 타이틀이 같으면 동일 객체")
     void testEqualsAndHashSetBehavior() {
-        LocalDate publishDate = LocalDate.of(2020, 1, 1);
+        LocalDateTime publishDate = LocalDateTime.of(2020, 1, 1, 10, 10, 10);
 
         Book book1 = new Book("1", publishDate, "asd");
         Book book2 = new Book("1", publishDate, "www");
@@ -41,10 +41,27 @@ class BookTest {
     }
 
     @Test
+    @DisplayName("같은 날짜와 다른 시간 비교")
+    void testDifferentDate() {
+        LocalDateTime publishDate = LocalDateTime.of(2020, 1, 1, 10, 10, 10);
+        LocalDateTime publishDate2 = LocalDateTime.of(2020, 1, 1, 9, 9, 9);
+        Book book1 = new Book("1", publishDate, "asd");
+        Book book2 = new Book("1", publishDate2, "www");
+
+        assertEquals(book1, book2);
+
+        // HashSet에 넣으면 중복으로 처리되어 크기는 1이어야 함
+        Set<Book> set = new HashSet<>();
+        set.add(book1);
+        set.add(book2);
+        assertEquals(1, set.size());
+    }
+
+    @Test
     @DisplayName("깊은 복사 진행")
     void testCloneCreatesDeepCopy() {
         //Date originalDate = ;
-        Book original = new Book("Original", LocalDate.now(), "Original comment");
+        Book original = new Book("Original", LocalDateTime.now(), "Original comment");
         Book cloned = original.clone();
 
         // 내용은 같아야 함
