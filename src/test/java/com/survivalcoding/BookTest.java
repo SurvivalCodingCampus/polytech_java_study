596d1fe2844082d81aae2839b155374c3d5cb5db
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-07-07 16:32:25 +0900

    feat : test code2 수정

--- src/test/java/com/survivalcoding/BookTest.java
+++ src/test/java/com/survivalcoding/BookTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,5 +28,26 @@ class BookTest {
 
         //then
         assertEquals(0, bookList.size());
+    }
+
+    @Test
+    @DisplayName("제목과 출간일이 같으면 같은 책으로 판단한다 List")
+    void BookTest2() {
+
+        //given
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String formatDate = sdf.format(date);
+        Book book1 = new Book("ABC", date, "NoComment");
+        Book book2 = new Book("ABC", date, "No");
+
+        Set<Book> bookSet = new HashSet<>() {
+        };
+
+        bookSet.add(book1);
+        bookSet.add(book2);
+
+        //then
+        assertEquals(1, bookSet.size());
     }
 }
