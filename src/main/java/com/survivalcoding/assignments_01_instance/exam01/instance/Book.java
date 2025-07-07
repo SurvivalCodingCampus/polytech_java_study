49d7d67853b78cb6fa2726e5d5e778290a2d9742
MJ37031 <rlaalswl0520@naver.com>
2025-07-07 17:08:36 +0900

    feat: publishDate를 Date로 받음

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/instance/Book.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/instance/Book.java
@@ -1,11 +1,12 @@
 package com.survivalcoding.assignments_01_instance.exam01.instance;
 
-import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class Book implements Comparable<Book>, Cloneable {
     private String title;
-    private LocalDateTime publishDate;
+    private Date publishDate;
     private String comment;
 
     public String getTitle() {
@@ -16,18 +17,23 @@ public class Book implements Comparable<Book>, Cloneable {
         this.title = title;
     }
 
-    public LocalDateTime getPublishDate() {
+    public Date getPublishDate() {
         return publishDate;
     }
 
-    public void setPublishDate(LocalDateTime publishDate) {
+    public void setPublishDate(Date publishDate) {
         this.publishDate = publishDate;
     }
 
-    public Book(String title, LocalDateTime publishDate) {
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Book(String title, Date publishDate) {
         this.title = title;
         this.publishDate = publishDate;
     }
+
 
     @Override
     public String toString() {
@@ -41,22 +47,38 @@ public class Book implements Comparable<Book>, Cloneable {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(publishDate.toLocalDate(), book.publishDate.toLocalDate());
+
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+        c1.setTime(this.publishDate);
+        c2.setTime(book.publishDate);
+
+        return Objects.equals(title, book.title) && c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR)
+                && c1.get(Calendar.MONTH) == c2.get(Calendar.MONTH)
+                && c1.get(Calendar.DAY_OF_MONTH) == c2.get(Calendar.DAY_OF_MONTH);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, publishDate.toLocalDate());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(publishDate);
+        return Objects.hash(title,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
     }
 
     @Override
     public int compareTo(Book o) {
+
         return publishDate.compareTo(o.publishDate) * -1;
     }
 
     @Override
     public Book clone() {
-        Book newBook = new Book(getTitle(), getPublishDate());  // 이게 복사
+        Book newBook = new Book(title);
+        newBook.publishDate = (Date) this.publishDate.clone();
+
         return newBook;
     }
 }
