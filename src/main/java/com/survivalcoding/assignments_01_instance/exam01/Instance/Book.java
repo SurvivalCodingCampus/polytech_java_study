e1587092b9b5f258d4efd8c5d210b4f1ec5a64de
Jiyun.Jung <modogo3@gmail.com>
2025-07-07 15:20:50 +0900

    Book 클래스 추가

--- src/main/java/com/survivalcoding/assignments_01_instance/exam01/Instance/Book.java
+++ src/main/java/com/survivalcoding/assignments_01_instance/exam01/Instance/Book.java
@@ -1,13 +1,22 @@
 package com.survivalcoding.assignments_01_instance.exam01.Instance;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
-    private Date publishDate;
+    private LocalDateTime publishDate;
     private String comment;
+
+    // 제목과 출간일이(예: 2024-01-01) 같으면 같은 책으로 판단한다. 또한 List, Map, Set 등의 컬렉션에 넣어도 동일 객체로 판단한다.
+public Book(String title, LocalDateTime publishDate, String comment){
+
 }
 
-// 제목과 출간일이(예: 2024-01-01) 같으면 같은 책으로 판단한다. 또한 List, Map, Set 등의 컬렉션에 넣어도 동일 객체로 판단한다.
-// Book 인스턴스를 담고 있는 컬렉션에 대해 Collections.sort() 를 사용하면 출간일이 신상 순서대로 정렬된다
-// clone() 메서드를 제공하고, 깊은 복사를 수행한다
+    // Book 인스턴스를 담고 있는 컬렉션에 대해 Collections.sort() 를 사용하면 출간일이 신상 순서대로 정렬된다
+    @Override
+    public int compareTo(Book book) {
+        return book.publishDate.compareTo(this.publishDate);
+    }
+}
+
+//clone() 메서드를 제공하고, 깊은 복사를 수행한다
