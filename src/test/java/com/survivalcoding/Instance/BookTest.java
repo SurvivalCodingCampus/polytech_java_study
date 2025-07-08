e10636e86cf9080321c30cde0c51ab12b894b48b
Soso-Kiki <polytechid10@gmail.com>
2025-07-08 10:28:18 +0900

    feat: 'Test'코드 추가

--- src/test/java/com/survivalcoding/Instance/BookTest.java
+++ src/test/java/com/survivalcoding/Instance/BookTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -66,5 +63,40 @@ class BookTest {
 
     }
 
+    @Test
+    @DisplayName("제목과 출간일(ex: 2024-01-01)이 같으면 같은 책으로 판단한다")
+    void shouldCOnsiderBooksWithSameTitleAndPublicationDateAsEqual() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2024);
+        calendar.set(Calendar.MONTH, 1);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR, 1);
+        calendar.set(Calendar.MINUTE, 1);
+
+        Date date1 = calendar.getTime();
+
+        calendar.set(Calendar.MINUTE, 2);
+
+        Date date2 = calendar.getTime();
+
+        Book book1 = new Book("제목", date1, "Comment1");
+        Book book2 = new Book("제목", date2, "Comment2");
+
+
+        System.out.println(book1.getPublishDate().equals(book2.getPublishDate()));
+        assertEquals(book1, book2);
+
+        List<Book> books = new ArrayList<>();
+        books.add(book1); // 1
+        books.remove(book2); // 0
+        assertEquals(0, books.size());
+
+        Set<Book> bookSet = new HashSet<>();
+        bookSet.add(book1);   // 1
+        bookSet.remove(book2);    // 0
+        assertEquals(0, bookSet.size());
+
+
+    }
 
 }
